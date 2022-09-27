c142f9e5f26750b0d9ef52670e6146adb92d9bfe
3bdalazeez <191021@ppu.edu.ps>
2022-09-27 14:00:38 +0300

    first try

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+
+  const [name, setName] = useState('abd');
+  /**
+   * 
+   * @param {React.FormEvent<HTMLFormElement>} e 
+   */
+  const submitHandler = e => {
+    e.preventDefault();
+
+    /**
+     * @type {HTMLFormElement}
+     */
+    const target = e.target;
+    console.log(target);
+  };
+
+
   return (
-    <form>
-      This is a form element
+    <form onSubmit={submitHandler}>
+      <input type="text" name="item-name" onChange={e => setName(e.target.value)} value={name} />
+      <button type='submit'>Add Item</button>
     </form>
   );
 };
