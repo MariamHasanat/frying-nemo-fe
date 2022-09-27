2a77d057c0f3a740a684d9f2bc239a233b86eb52
raghad__ns <raghadnadersalem2002017@GMAIL.COM>
2022-09-27 14:01:33 +0300

    first commit

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+  const [name , setName] = useState ('Raghad') ;
+
+  /**
+   * 
+   * @param {React.ChangeEvent<HTMLInputElement>} e   //Event object
+   */
+   const handle = e => {
+    e.preventDefault() ;
+    console.debug (name) ;
+   }
   return (
     <form>
-      This is a form element
+      <input type="text" 
+      name =  'name' 
+      placeholder='name' 
+      value = {name} 
+      onChange = {e => setName (e.target.value)}
+      />
+
+      <div><button type='submit'  
+      onClick={handle}> Create</button></div>
     </form>
   );
 };
