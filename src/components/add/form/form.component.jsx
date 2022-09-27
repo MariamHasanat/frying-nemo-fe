f678fdc7e2b1f39f9f6d9fa16ceb60eb426416ce
Manolya <201097@ppu.edu.ps>
2022-09-27 14:00:04 +0300

    working on input in form

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+  const [name, setName] = useState('');
+
+  /**
+   * Form Submit Handler 
+   * @param {React.ChangeEvent<HTMLInputElement>} event
+   */
+
+  
+  
   return (
-    <form>
-      This is a form element
+    <form onSubmit={() => console.log('submited')}>
+      <input
+        type="text"
+        name="Name"
+        placeholder='Name'
+        value={''}
+        onChange={e => console.log(e.target.value)}
+      />
+      <div>
+        <button type='submit'>Create</button>
+      </div>
     </form>
   );
 };
