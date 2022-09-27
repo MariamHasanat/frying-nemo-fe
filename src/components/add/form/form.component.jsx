32210322078b4dd735ced7b19dea7ca598007277
hudafarhat <hu03@gmail.com>
2022-09-27 14:07:16 +0300

    change form component

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,34 @@
 import './form.css';
+import { useState } from 'react';
 
 const Form = (props) => {
+  const [name,setName] = useState('Huda');
+  /**
+   * Handler function for the form onSubmit event.
+   * @param {React.FormEvent<HTMLFormElement>} e Event object.
+   */
+  const submitHandler = e => {
+    e.preventDefault();
+
+    /**
+     * @type {HTMLFormElement}
+     */
+    const target = e.target;
+    console.debug(target);
+  };
+
   return (
-    <form>
-      This is a form element
+    <form className='add-form' onSubmit={submitHandler()}>
+      <input
+        type="text"
+        name='name'
+        placeholder='Name'
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <div style={{ marginTop: 20 }}>
+        <button type='submit'>Create</button>
+      </div>
     </form>
   );
 };
