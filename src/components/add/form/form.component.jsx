2edde493ca3bfb1bedf0a63d5f9d71ff7b703086
omar <227196@ppu.edu.ps>
2022-09-27 13:56:46 +0300

    Form Strcture

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+  const [name, setName] = useState(`Omar`)
+  /**
+   * 
+   * @param {React.FormEvent<HTMLFormElement>} e 
+   */
+  const submitHandler = e => {
+    e.preventDefault();
+    console.debug(e.target.name.value)
+  }
+
+
+  const chnageName = (value) => {
+    setName(value.target.value)
+  }
+
   return (
-    <form>
-      This is a form element
+    <form className='add-form' onSubmit={submitHandler}>
+      <input type="text" name='name' placeholder='Name' value={name} onChange={e => {
+        chnageName(e)
+      }}/>
+      <div>
+        <button type="submit">
+          Create
+        </button>
+      </div>
     </form>
   );
 };
