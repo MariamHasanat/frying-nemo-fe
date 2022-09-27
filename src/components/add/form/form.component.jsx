8b55d61d9cc5182140233a4c568b47e182ece9fe
Mariam <201011@ppu.ediu.ps>
2022-09-27 14:08:01 +0300

    form chnges

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,36 @@
+import React, { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
-  return (
-    <form>
-      This is a form element
+  const [name,setName]=useState('mariam');
+
+/**
+ * 
+ * @param {React.FormEvent<HTMLFormElement>} e event object
+ */
+
+const submitHandler= e  =>{
+  e.preventDefault();
+}
+/**
+ *  @type {HTMLFormElement}
+ */
+const target =e.target;
+
+
+return (
+    <form className='add-form' onSubmit={submitHandler}>
+    <input type='text'
+    name= "name"
+    placeholder='name'
+    value={name}
+    onChange={e =>setName( e.target.value)}
+    />
+      <div style ={{ marginTop:20}}>
+        <button type="submit">creat</button>
+    
+      </div>
+ 
     </form>
   );
 };
