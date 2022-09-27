2120a6ecbcc320ad9967f6e0f6746b452bcbd79c
Momen-G-Ar-Official <cp.momen@gmail.com>
2022-09-27 20:40:37 +0300

    Add onchange

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,11 +1,21 @@
 import './form.css';
-
+import { useState } from 'react';
 const Form = (props) => {
-  return (
-    <form>
-      This is a form element
-    </form>
-  );
+    const [name, setName] = useState('');
+    return (
+        <form className='form'>
+            <input
+                type="text"
+                name='name'
+                placeholder='Name'
+                value={name}
+                onChange={(e) => { setName(e.target.value); }}
+            />
+            <div className='sub'>
+                <input type="submit"/>
+            </div>
+        </form>
+    );
 };
 
 export default Form;
--- src/pages/add/add.component.jsx
+++ src/pages/add/add.component.jsx
@@ -3,12 +3,12 @@ import './add.css';
 import Form from '../../components/add/form/form.component';
 
 const AddPage = (props) => {
-  return (
-    <div>
-      <h1>Add a new item</h1>
-      <Form />
-    </div>
-  );
+    return (
+        <div className='add-form'>
+            <h1>Add a new item</h1>
+            <Form />
+        </div>
+    );
 };
 
 export default AddPage;
