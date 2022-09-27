adaa9890264229034c584646c732edaa19eea013
arwwd2002 <arwwd2002@gmail.com>
2022-09-27 14:01:37 +0300

    add form

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+
+  const [name, setName] = useState("Asia");
+  /**
+   * 
+   * @param {React.FormEvent<HTMLFormElement>} e Event Object. 
+   */
+  const submitHandler = e => {
+    e.preventDefault();
+
+    /**
+     * @type {HTMLFormElement}
+     */
+    const target = e.target;
+    console.debug(target);
+  };
   return (
-    <form>
-      This is a form element
+    <form className='add-form' onSubmit={submitHandler}>
+      <input
+        type="text"
+        name='name'
+        placeholder='Name'
+        value={name}
+        onChange={e => setName(e.target.value)} />
+      <div style={{ marginTop: 20 }}>
+        <button type='submit'>Create</button>
+      </div>
     </form>
   );
 };
