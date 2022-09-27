2130a69e22381c9ca3901efeac212227f6532b3a
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2022-09-27 20:21:40 +0300

    my first commit

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,10 +1,34 @@
+import { type } from '@testing-library/user-event/dist/type';
+import React, { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+const [name, setName]=useState('ayat');
+
+/** 
+* @parm {React.ChangeEvent<HTMLInputElement>} e event object.
+
+*/
+const submitHandeller=(e)=>{
+  e.preventDefault();
+/*
+ * @type {HTMLFormElement} 
+*/
+
+  const target = e.target;
+}
+
   return (
-    <form>
-      This is a form element
-    </form>
+    <form onSubmit={submitHandeller}>
+<input type="text"
+ name="name"
+ placeholder='Name'
+ value={name}
+ onChange={e => setName(e.target.value)}
+  />   
+  
+  <button type='submit'>create</button>
+   </form>
   );
 };
 
