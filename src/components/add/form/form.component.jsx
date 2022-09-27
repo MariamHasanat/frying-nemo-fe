f6e0b507bf1cf93497de081bbb624517d6c2afa7
dala_bilal <dala_bilal@gmail.com>
2022-09-27 14:00:20 +0300

    initiaol edit

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+  const [name , setName] = useState('');
+  const handler = e => {
+   e.preventDefault();
+    /**
+     * @type {HTMLformElement}
+     */
+    const target = e.target;
+    console.debug(target);
+  }
   return (
-    <form>
-      This is a form element
+    <form onSubmit={handler}>
+      <input 
+      type="text" 
+      value={name}
+      placeholder="name"
+      onChange={e => setName(e.target.value)}
+      />
+      <div style={{ margintop : 20 }}>
+        <button type='submit'>Creat</button>
+        </div>
     </form>
   );
 };
