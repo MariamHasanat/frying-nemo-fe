8024cd696793bea2e688a129a4d6c7a094b3cb07
moayadanajjar <anjjarmoayad@gmail.com>
2022-09-27 14:09:29 +0300

    create form

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+
+
+
+  const [name, SetName] = useState('add item')
+
+  const sumbitHandler = e => {
+
+   const target=e.target;
+   console.debug(target);
+
+}
+
   return (
-    <form>
-      This is a form element
+    <form className='item-page' onSubmit={sumbitHandler}>
+      <input type='text' name="name" placeholder="new item" value={name} onChange={e=>SetName(e.target.name)} ></input>
+      <br />
+<button type='sumbit'>Create</button>
     </form>
   );
 };
