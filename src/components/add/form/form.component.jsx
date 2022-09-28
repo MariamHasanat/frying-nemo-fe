6f72fb7ac767248b097d14206545fd5d06e65dbf
Manolya <201097@ppu.edu.ps>
2022-09-28 20:11:08 +0300

    form input is working and styled... further styling may be needed later on

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -6,19 +6,26 @@ const Form = (props) => {
 
   /**
    * Form Submit Handler 
-   * @param {React.ChangeEvent<HTMLInputElement>} event
+   * @param e{React.ChangeEvent<HTMLInputElement>} event
    */
+  const submitHandler = (e) => {
+    e.preventDefault();
+    /**
+     * @type {HTMLFormElement}
+     */
+    const target = e.target;
+    //  console.log(e.target.uName.value);
+    console.log(target.Name.value);
+  };
 
-  
-  
   return (
-    <form onSubmit={() => console.log('submited')}>
+    <form onSubmit={submitHandler} className="styled-form">
       <input
         type="text"
         name="Name"
-        placeholder='Name'
-        value={''}
-        onChange={e => console.log(e.target.value)}
+        placeholder="Name"
+        value={name}
+        onChange={e => setName(e.target.value)}
       />
       <div>
         <button type='submit'>Create</button>
--- src/pages/add/add.component.jsx
+++ src/pages/add/add.component.jsx
@@ -4,10 +4,11 @@ import Form from '../../components/add/form/form.component';
 
 const AddPage = (props) => {
   return (
-    <div>
-      <h1>Add a new item</h1>
-      <Form />
-    </div>
+    <><h1>Add a new item</h1>
+      <div className='add-page'>
+        <Form />
+      </div>
+    </>
   );
 };
 
