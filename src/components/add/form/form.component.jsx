f3d1eff23f9e04ec39dd3e01344eb9dd976af27a
sajedaswad <swadsajeda@gmail.com>
2022-09-29 17:12:21 +0200

    goo

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './form.css';
+const Form =(props)=>{
+ 
+  const handleSubmit=(e)=>{
+    e.preventDefult();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+    console.log(e.target.passward.value);
+  }
+return(
+  <form className='form' onSubmit={handleSubmit}>
+    <div className='input'>
+      <input
+       type="text" name="name" placeholder='Sajeda'
+       ></input>
+      <input 
+      type="email" name="email" placeholder='sajeda@example.com'
+      ></input>
+      <input type="password" name="passward" placeholder='.......'  outoComplete='true'></input>
 
-const Form = (props) => {
-  return (
-    <form>
-      This is a form element
-    </form>
-  );
+   </div>
+    <input  className='submit' type='submit' value='SignUp'></input>
+  </form>
+);
 };
-
 export default Form;
