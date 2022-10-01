e8c43d12be31bef5e9d1e9883f78a6874fd2fbdc
Nadeen Jubeh <191080@ppu.edu.ps>
2022-10-01 20:47:17 +0300

    format

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -2,32 +2,32 @@ import { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
-  const[name,setName]=useState('Nadeen');
+  const [name, setName] = useState('Nadeen');
   /**
    * 
    * @param {React.FormEvent<HTMLFormElement>} e 
    */
-  const SubmitHandel=e=>{
+  const SubmitHandel = e => {
     e.preventDefult();
-/**
- * @type {HtmlFormElement}
- */
+    /**
+     * @type {HtmlFormElement}
+     */
     const target = e.target;
 
-  }
+  };
   return (
     <form className='add-form' onSubmit={SubmitHandel}>
-       <input type="text"
+      <input type="text"
         placeholder='Name'
         value={name}
         name='name'
-       onChange={e=>setName(e.target
-        .value)}
-       /> 
-       <div>
+        onChange={e => setName(e.target
+          .value)}
+      />
+      <div>
         <button type='submit'>Create</button>
-       </div>
-       </form>
+      </div>
+    </form>
   );
 };
 
