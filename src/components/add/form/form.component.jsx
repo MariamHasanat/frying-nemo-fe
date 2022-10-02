0debb6cdf841b4f663c6e10d49f123622d465587
dala_bilal <dala_bilal@gmail.com>
2022-10-02 13:53:47 +0300

    last edit

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './form.css';
+import Input from '../../../common/input/input.common';
 
 const Form = (props) => {
-  const [name , setName] = useState('');
+  const [name , setName] = useState('dala');
   const handler = e => {
    e.preventDefault();
     /**
@@ -11,16 +12,42 @@ const Form = (props) => {
     const target = e.target;
     console.debug(target);
   }
+
+  const onNamech = e => {
+
+    let val = e.target.value;
+
+    if(val.includes('.')) {
+
+      val = val.replace('.','');
+
+    }
+
+    if(/find/ig.test(val)) {
+
+      val = val.replace(/find/ig , 'fry');
+
+    }
+
+    if (val.length > 20) {
+
+      alert ('charecter limit excedded') ;
+
+      val = val.substring (0 , 20)  ;
+
+    }
+    setName(val);
+  }
+
   return (
     <form className='add-form' onSubmit={handler}>
-      
-      <input 
-        type="text" 
-        value={name}
-        placeholder="name"
-        onChange={e => setName(e.target.value)}
-      />
+
       <div style={{ margintop : 20 }}>
+        <Input
+         label="Name"
+         value={name}
+         onChange={onNamech}
+        />
         <button className='btn' type='submit'>Creat</button>
         </div>
     </form>
