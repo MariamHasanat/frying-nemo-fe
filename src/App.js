0ad993e61dbe58bebd037bda110d86f41d3940ba
qjabari <qjabari@sadasol.com>
2022-10-04 12:45:01 +0300

    Adding website header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from "./components/core/header/header.componet";
 import AddPage from "./pages/add/add.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <AddPage />
     </div>
   );
