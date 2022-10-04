bd027545690a3dc4516978a82279d1b2eaff81bb
dala_bilal <dalahhashlamoon@gmail.com>
2022-10-05 00:54:33 +0300

    select edit

--- src/common/Select/Select.jsx
+++ src/common/Select/Select.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import './select.css'
 
 const Select = (props) => {
-  const { label, ...optionProps } = props;
+  const { label, ...selectProps } = props;
   return (
-   
-       label ? (
-          <label>
-            <span>{label}</span>
-            &nbsp;
-            {optionProps.required && <span>*</span>}
-          </label>) : null
-      
-     <select {...optionProps}/>
+   <div>
+     {
+         label ? (
+            <label>
+              <span>{label}</span>
+              &nbsp;
+              {selectProps.required && <span>*</span>}
+            </label>) : null}
+     
+        <select {...selectProps} />
+   </div>
     
   )
 }
--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -5,59 +5,66 @@ import Textarea from '../../../common/textarea/textarea.component';
 import Select from '../../../common/Select/Select';
 
 const Form = (props) => {
-  const [name , setName] = useState('dala');
+  const [name, setName] = useState('dala');
+  const [number, setNumber] = useState();
 
   /**
    * Handles on change events on the name field.
    * @param {React.ChangeEvent<HTMLInputElement>} e On change event object.
    */
   const handler = e => {
-   e.preventDefault();
+    e.preventDefault();
     /**
      * @type {HTMLformElement}
      */
     const target = e.target;
     console.debug(target);
-  }
+  };
 
   const onNamechange = e => {
 
     let val = e.target.value;
 
-    if(val.includes('.')) {
-
-      val = val.replace('.','');
-
+    if (val.includes('.')) {
+      val = val.replace('.', '');
     }
 
-    if(/find/ig.test(val)) {
-
-      val = val.replace(/find/ig , 'fry');
-
+    if (/find/ig.test(val)) {
+      val = val.replace(/find/ig, 'fry');
     }
 
     if (val.length > 20) {
-
-      alert ('charecter limit excedded') ;
-
-      val = val.substring (0 , 20)  ;
-
+      alert('charecter limit excedded');
+      val = val.substring(0, 20);
     }
     setName(val);
-  }
+  };
+
+  const onNumberChange = e => {
+
+      let value = e.target.value;
+      setNumber(value);
+  };
 
   return (
     <form className='add-form' onSubmit={handler}>
-      <div style={{ margintop : 20 }}>
+      <div style={{ margintop: 20 }}>
         <Input
-         label="Name"
-         value={name}
-         onChange={onNamechange}
-         required
+          label="Name"
+          value={name}
+          onChange={onNamechange}
+          required
         />
-       
+        <Input
+          label="Price"
+          value={number}
+          type="number"
+          min={0}
+          required
+        />
+
         <Textarea
-        label="Describtion"
+          label="Describtion"
         />
         <Select label="Category">
           <option>Fish</option>
@@ -67,10 +74,10 @@ const Form = (props) => {
           <option>Sandwiches</option>
           <option>Main Dish</option>
           <option>Appetizers</option>
-          <option>Ice Cream</option> 
+          <option>Ice Cream</option>
         </Select>
         <button className='btn' type='submit'>Creat</button>
-        </div>
+      </div>
     </form>
   );
 };
