a72c0e15a365420a9850914ecb2eeb792d214122
qjabari <qjabari@sadasol.com>
2022-10-04 13:54:23 +0300

    Adding select area element

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Input from '../../common/input/input.component';
+import Select from '../../common/select/select.component';
 import Textarea from '../../common/textarea/textarea.component';
 import './form.css';
 
@@ -39,6 +40,17 @@ const Form = (props) => {
     setName(value);
   };
 
+  const categories = [
+    'Fish',
+    'Drinks',
+    'Hookah',
+    'Salads',
+    'Sandwiches',
+    'Main Dish',
+    'Appetizers',
+    'Ice Cream'
+  ];
+
   return (
     <form className="addForm" onSubmit={submitHandler} >
       <Input
@@ -56,6 +68,11 @@ const Form = (props) => {
         min={0}
         required
       />
+      <Select label="Category" required>
+        {categories.map(item => {
+          return <option key={item} value={item}>{item}</option>;
+        })}
+      </Select>
       <div className="addFormButtons">
         <button type="submit">Create</button>
       </div>
