39f464ba4bf54872b51c5f5f6333ca3df3d0c0f6
Momen-G-Ar-Official <cp.momen@gmail.com>
2022-10-05 17:45:34 +0300

    add the header component

---
+++ src/components/common/header/header.component.jsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src='./images/nemoInHeader.png' alt="nemo image" />
+            <h2>
+                Frying Nemo 
+            </h2>
+        </div>
+    );
+};
+export default Header;
--- src/pages/add/add.component.jsx
+++ src/pages/add/add.component.jsx
@@ -1,10 +1,12 @@
 import './add.css';
 
 import Form from '../../components/add/form/form.component';
+import Header from '../../components/common/header/header.component';
 
 const AddPage = (props) => {
     return (
         <div className='add-form'>
+            <Header/>
             <h1>Add a new item</h1>
             <Form />
         </div>
