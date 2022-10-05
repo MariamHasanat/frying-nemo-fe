6f0034d5be0407d361cd4d37b635be52105aef6c
abdullah-home <181004@ppu.edu.ps>
2022-10-05 18:23:52 +0300

    add header and render catigories list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import AddPage from "./pages/add/add.component";
+import Header from "./components/common/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <AddPage />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
---
+++ src/components/common/header/header.component.jsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+const Header=()=>{
+
+  return(
+    <div className='header-container'>
+      <div className='logo'></div>
+      <span className='resturant-name'>Frying Nemo</span>
+    </div>
+  );
+};
+
+export default Header;
