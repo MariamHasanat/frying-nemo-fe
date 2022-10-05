554d8010bdabad3a9ac87f468e47b603592b5e17
dala_bilal <dalahhashlamoon@gmail.com>
2022-10-06 00:04:36 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import AddPage from "./pages/add/add.component";
+import Header from "./logo/Header";
 
 function App() {
   return (
     <div>
+      <Header/>
       <AddPage />
     </div>
   );
---
+++ src/logo/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = props => {
+  return (
+    <header >
+      <div className="Header">
+       <h1>
+        <img src="./res.png" alt="" />
+         Frying Nemo
+       </h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
