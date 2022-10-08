ca3ca14bf3e6fce99c4621919af7649fcd09e5c0
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2022-10-09 00:05:24 +0300

    clear multi value input feild

--- src/components/common/multi-value-input/multi-value-input.jsx
+++ src/components/common/multi-value-input/multi-value-input.jsx
@@ -24,7 +24,7 @@ const MultivalueInput = props => {
     if (newItem.trim().length > 0 && !props.value.includes(newItem)) {
       props.onChange([...props.value, newItem]);
     }
- 
+    setNewItem("");
 
   };
 
