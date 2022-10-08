d0f943005cca6b0d63977d9b670a72ab8d2e8906
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2022-10-08 23:57:03 +0300

    adding remove function to the list

--- src/components/common/multi-value-input/multi-value-input.jsx
+++ src/components/common/multi-value-input/multi-value-input.jsx
@@ -14,19 +14,32 @@ import './multi-value-input.css';
  * 
  */
 
+
+
 const MultivalueInput = props => {
 
   const [newItem, setNewItem] = useState('');
 
   const addItem = () => {
-    if(newItem.trim().length >0 && !props.value.includes(newItem))
-    {
+    if (newItem.trim().length > 0 && !props.value.includes(newItem)) {
       props.onChange([...props.value, newItem]);
     }
+ 
+
   };
 
-const delet =(index)=> {
-}
+  const deleted = (removedItem) => {
+
+    var Myitems = [...props.value];
+    var index = Myitems.indexOf(removedItem);
+    if (index >= 0) {
+      Myitems.splice(index, 1);
+    }
+    props.onChange(Myitems);
+
+  };
+
+
   return (
     <div className="multivalue">
 
@@ -41,21 +54,21 @@ const delet =(index)=> {
       </div>
       {
 
-          props.value.length > 0 && (
-           <ul>
-              {props.value.map(item => {
-                return (
-                  <li key={item} >
-                    <span>{item}</span>
-                    <button type="button" onClick={()=>delet(item)}>&times;</button>
-                  </li>
-                );
-              })}
-            </ul>
-          )
-          
+        props.value.length > 0 && (
+          <ul>
+            {props.value.map(item => {
+              return (
+                <li key={item} >
+                  <span>{item}</span>
+                  <button type="button" onClick={() => deleted(item)}>&times;</button>
+                </li>
+              );
+            })}
+          </ul>
+        )
+
       }
-       
+
     </div>
   );
 
