11a3c6e02f695763d5562ce7e7a31c25735e090b
abdullah <181004@ppu.edu.ps>
2022-10-09 12:37:25 +0300

    Adding removeItem

--- src/components/multi-value-input.jsx/multivalue-input.component.jsx
+++ src/components/multi-value-input.jsx/multivalue-input.component.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
-import Input from '../common/input/input.component';
-import './multivalue-input.css';
-import '../../common.css';
+import { useState } from "react";
+import Input from "../common/input/input.component";
+import "./multivalue-input.css";
+import "../../common.css";
 /**
  * @param {{
  *  label?: string;
  *  value: string[];
  *  onChange: (value: string[]) => void;
- * }} props 
+ * }} props
  */
 const MultivalueInput = (props) => {
-  const [newItemValue, setNewItemValue] = useState('');
+  const [newItemValue, setNewItemValue] = useState("");
   const addItem = () => {
     if (newItemValue.trim().length > 0 && !props.value.includes(newItemValue)) {
       props.onChange([...props.value, newItemValue]);
     }
-  };
-  const removeItem = (index) => {
-    props.value.splice(index, 1);
   };
   return (
     <div className="multivalueInputWrapper">
@@ -25,31 +22,34 @@ const MultivalueInput = (props) => {
         <Input
           label={props.label}
           value={newItemValue}
-          onChange={e => setNewItemValue(e.target.value)}
+          onChange={(e) => setNewItemValue(e.target.value)}
         />
-        <button
-          className="nemo-button"
-          type="button"
-          onClick={addItem}
-        >
+        <button className="nemo-button" type="button" onClick={addItem}>
           Add
         </button>
       </div>
-      {
-        props.value.length > 0 && (
-          <ul>
-            {props.value.map(item => {
-              return (
-                <li key={item} >
-                  <span>{item}</span>
-                  <button type="button" className='remove-btn'
-                    onClick={removeItem(props.value.indexOf(item))}>&times;</button>
-                </li>
-              );
-            })}
-          </ul>
-        )
-      }
+      {props.value.length > 0 && (
+        <ul>
+          {props.value.map((item) => {
+            return (
+              <li key={item}>
+                <span>{item}</span>
+                <button
+                  type="button"
+                  className="remove-btn"
+                  onClick={() => {
+                    const valueCopy = [...props.value];
+                    valueCopy.splice(valueCopy.indexOf(item), 1);
+                    props.onChange(valueCopy);
+                  }}
+                >
+                  &times;
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
