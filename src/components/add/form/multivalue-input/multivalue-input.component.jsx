7df2ae055ee955fd571cd2f57fbfcdd9e352e7a2
hudafarhat <hu03@gmail.com>
2022-10-09 12:35:48 +0300

    delete item

--- src/components/add/form/multivalue-input/multivalue-input.component.jsx
+++ src/components/add/form/multivalue-input/multivalue-input.component.jsx
@@ -15,46 +15,49 @@ const MultivalueInput = props => {
 
   const addItem = () => {
     if (newItemValue.trim().length > 0 && !props.value.includes(newItemValue)) {
-      props.onChange([...props.value, newItemValue]);
+      const valueAfterAddition = [...props.value, newItemValue];
+      props.onChange(valueAfterAddition);
     }
   };
 
-  const deleteItem = (key) => {
-    
-            
+  const deleteItem = (item) => {
+    const valueAfterRemoval = props.value.filter(element => element !== item);
+    props.onChange(valueAfterRemoval);
+
   };
-  return (
-    <div className="multivalueInputWrapper">
-      <div className="controls">
-        <Input
-          label={props.label}
-          value={newItemValue}
-          onChange={e => setNewItemValue(e.target.value)}
-        />
-        <button
-          className="nemo-button"
-          type="button"
-          onClick={addItem}
-        >
-          Add
-        </button>
-      </div>
-      {
-        props.value.length > 0 && (
-          <ul>
-            {props.value.map(item => {
-              return (
-                <li key={item} >
-                  <span>{item}</span>
-                  <button type="button" onClick={deleteItem(item)}>&times;</button>
-                </li>
-              );
-            })}
-          </ul>
-        )
-      }
+return (
+  <div className="multivalueInputWrapper">
+    <div className="controls">
+      <Input
+        label={props.label}
+        value={newItemValue}
+        onChange={e => setNewItemValue(e.target.value)}
+      />
+      <button
+        className="nemo-button"
+        type="button"
+        onClick={addItem}
+      >
+        Add
+      </button>
     </div>
-  );
+    {
+      props.value.length > 0 && (
+        <ul>
+          {props.value.map(item => {
+            return (
+              <li key={item} >
+                <span>{item}</span>
+                <button type="button" onClick={deleteItem(item)}>
+                  &times;</button>
+              </li>
+            );
+          })}
+        </ul>
+      )
+    }
+  </div>
+);
 };
 
 export default MultivalueInput;
