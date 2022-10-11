7728106ed89df410d686a0d233b08421b6e3ad15
nadeenj <191080@ppu.edu.ps>
2022-10-11 13:53:32 +0300

    Add current Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import AddPage from "./pages/add/add.component";
+import ViewPage from "./common/view/view.component";
 import Header from'./common/header/header.component';
+import { useState } from "react";
 
 function App() {
+  const [currentPage,setCurrentPage]=useState('add');
+  const changePage=(nwePage)=>{
+      setCurrentPage(nwePage);
+  }
   return (
     <div>
-      <Header />
-      <AddPage />
+
+      <Header onNavigate ={changePage} currentPage={currentPage} />
+      {currentPage === 'add' && <AddPage onNavigate={changePage} />}
+      {currentPage === 'view' && <ViewPage onNavigate={changePage} />}
     </div>
   );
 }
