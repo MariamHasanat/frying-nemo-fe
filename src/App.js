241a9ae1e75062abcab996dab803974cf4f7b029
Momrn-G-AR <cp.momen@gmail.com>
2022-10-11 13:56:25 +0300

    add two buttons add page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import Header from "./components/common/header/header.component";
 import AddPage from "./pages/add/add.component";
+import View from "./pages/view/view";
+
 
 function App() {
+    const [curPage, setCurPage] = useState('add');
+
+    const changePage = (newPage) => {
+        setCurPage(newPage);
+    };
+
     return (
         <div>
-            <Header/>
-            <AddPage />
+            <Header
+                onNavigate={changePage}
+                currentPage={curPage}
+            />
+            {
+                (curPage === 'add')
+                    ? <AddPage />
+                    : <View />
+            }
+
         </div>
     );
 }
