bf76718342a676ed3e7da5d3bee47a0b8a4b37ee
Mariam <201011@ppu.ediu.ps>
2022-10-16 13:18:48 +0300

    import item from view

--- src/pages/view/view.container.jsx
+++ src/pages/view/view.container.jsx
@@ -1,9 +1,11 @@
 import './viewContainerStyle.css';
+import './item/item.jsx'
 
 const ViewPage = (props) => {
   return (
     <div className='view-container'>
       <h1 className='h1'>View Menu Items</h1>
+      <Item />
     </div>
   );
 };
