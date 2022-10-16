d53eb0b7a7ba5dfe91d238e7a0b4f3dfaeea6d8e
nadeenj <191080@ppu.edu.ps>
2022-10-16 13:53:04 +0300

    Add class item

--- src/common/view/view.component.jsx
+++ src/common/view/view.component.jsx
@@ -1,9 +1,21 @@
 import './view.css';
-
+import Items from '../view/item/item.component';
+import { useState } from "react";
+const getMenu = ()=>JSON.parse(localStorage.menuItems|| '[]');
 const ViewPage = (props) => {
+  /**
+   * @type {{Array,Function}}
+   * 
+   */
+  const[menuItems]=useState(getMenu());
   return (
     <div className="view-page">
       <h1>View All Menu Items</h1>
+      <div className="item">
+        {
+          menuItems.map((item,index )=> <Items data={item} key={item.name + index}/>)
+        }
+      </div>
     </div>
   );
 };
