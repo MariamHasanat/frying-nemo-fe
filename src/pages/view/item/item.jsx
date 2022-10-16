61fc9a3afe4098c2c0048709398b6d40b213c822
Mariam <201011@ppu.ediu.ps>
2022-10-16 13:09:47 +0300

    add item component body

---
+++ src/pages/view/item/item.jsx
@@ -0,0 +1,28 @@
+import './item.css';
+
+const Item = (props) => {
+
+  return (
+    <div className="item-card">
+      <div className="img">
+        <img src="https://i.imgur.com/eFWRUuR.jpg" alt="food" />
+      </div>
+      <div className="info">
+        <h2>Double Cheese Potato Burger</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque dicta laudantium.</p>
+        <p className="ingredients">Beef, Bread, Tomato, Fries, Lectus</p>
+        <hr />
+      </div>
+      <div className="price">
+        <span>25.5$</span>
+        <div className="add-cart">
+          <button>+</button>
+          <input type="number" max={500} />
+          <button>-</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
