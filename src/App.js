39d1024ea148d61acc90273d7bf858d9131f6d10
dala_bilal <dala@gmail.com>
2022-10-18 12:16:12 +0300

    DStart with useeffect

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import AddPage from "./pages/add/add.component";
 import { useState } from "react";
 import Header from "./logo/Header";
 import ViewPage from "./pages/view/ViewPage";
+import Notfound from "./pages/not-found/Notfound";
 import './common.css';
 
 function App() {
-  const [CurretnPage, setCurrentPage] = useState('add');
+  const [CurretnPage, setCurrentPage] = useState('404');
   return (
     <div>
       <Header curretnPage={CurretnPage}
@@ -14,6 +15,7 @@ function App() {
       {CurretnPage === 'add' && <AddPage
         onNavigate={(changePage) => setCurrentPage(changePage)} />}
       {CurretnPage === 'view' && <ViewPage />}
+      {CurretnPage === '404' && <Notfound />}
       {/* {currentPage === '404' && <NotFoundPage />} */}
     </div>
   );
---
+++ src/pages/not-found/Result.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+// import React from 'react'
+
+const Result = (props) => {
+  const[win , setWin] = useState(false);
+  const[color , setColor] = useState();
+
+  useEffect(() => {
+    if(props.counter >= 10) {
+      setWin(true);
+    }
+    setColor();
+  }, [props.counter]);
+
+  return (
+    <div>
+     {
+      win && <h3>you are the winner</h3>
+     }
+    
+   </div>
+  )
+}
+
+export default Result;
