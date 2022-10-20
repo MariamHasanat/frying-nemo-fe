f7463d9456fc869b1311cec1519c89bd3368060e
raghad__ns <raghadnadersalem20022017@gmail.com>
2022-10-20 13:04:31 +0300

    adding timer

--- src/pages/add/add.component.jsx
+++ src/pages/add/add.component.jsx
@@ -1,11 +1,25 @@
 import './add.css';
 
 import Form from '../../components/add/form/form.component';
+import { useEffect, useState } from 'react';
 
 const AddPage = (props) => {
+  const [time , setTime] = useState (new Date()) ;
+  const updateTimer = () => setTime (new Date()) 
+  useEffect (()=> {
+    //This code excuting when the component did mount 
+    const timer = setInterval (() => {
+      console.log  ('time running out') ;
+      // updateTimer ;
+      setTime (new Date()) ; 
+    }, 1000)
+    return (() => clearInterval (timer) )
+  } , [])
+
   return (
     <div className='add'>
       <h1>Add a new item</h1>
+      <span>{time.toLocaleTimeString()}</span>
       <Form onNavigate = {props.onNavigate} 
       onAdd = {props.onAdd}/>
     </div>
