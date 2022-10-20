3a206dbf7ca9fc0e0a042d6c0cb842511ce331cf
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2022-10-20 13:24:28 +0300

    add clock timer

--- src/pages/add/add.component.jsx
+++ src/pages/add/add.component.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react';
 import Form from '../../components/add/form/form.component';
 import './add.css';
 
 const AddPage = (props) => {
+
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.debug('use Effect executing > starting timer');
+    const timer = setInterval(updatTimer, 1000);
+    return () => {
+
+      console.debug("use Effect cleaning up > clear the timer");
+      clearInterval(timer);
+    };
+
+
+  }, []);
+
+  const updatTimer = () => {
+    console.log("update timer is Executnig");
+    setTime(new Date());
+  };
   return (
     <div>
+<span>{time.toLocaleTimeString()}</span>
       <h1>Add a new item</h1>
       {/* <Form onNavigate={props.onNavigate} /> */}
-     <Form onNavigate={props.onNavigate}/>
- 
+
+      <Form onNavigate={props.onNavigate} />
+
     </div>
   );
 };
