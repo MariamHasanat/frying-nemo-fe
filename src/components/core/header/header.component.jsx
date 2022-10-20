93c9928abe51ac185499f98849f2da4f26f7ff6e
AsiaShalaldeh <arwwd2002@gmail.com>
2022-10-20 12:57:31 +0300

    adding Timer

--- src/components/core/header/header.component.jsx
+++ src/components/core/header/header.component.jsx
@@ -1,17 +1,23 @@
-import './header.css';
+import { useEffect, useState } from "react";
+import "./header.css";
 
 const Header = (props) => {
   return (
-    <div className='main-class'>
-      <div className='logo'>
-        <img src={process.env.PUBLIC_URL + '/logo.png'} alt="" width={70} />
+    <div className="main-class">
+      <div className="logo">
+        <img src={process.env.PUBLIC_URL + "/logo.png"} alt="" width={70} />
         <p>Frying Nemo</p>
       </div>
       <h1>Add new Item</h1>
       <div className="right">
         <nav>
-          <button className={props.currentPage === 'add' ? 'current' : ''} onClick={() => props.onNavigate('add')}>Add</button>
-          <button onClick={() => props.onNavigate('view')}>View</button>
+          <button
+            className={props.currentPage === "add" ? "current" : ""}
+            onClick={() => props.onNavigate("add")}
+          >
+            Add
+          </button>
+          <button onClick={() => props.onNavigate("view")}>View</button>
         </nav>
       </div>
     </div>
