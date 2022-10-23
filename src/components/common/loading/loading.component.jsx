83cc6d560830369d0916a8a6fbd2a720ea342217
ManolyaTam <tamimimanolya@gmail.com>
2022-10-23 18:21:44 +0300

    added loading component and used it in view page

---
+++ src/components/common/loading/loading.component.jsx
@@ -0,0 +1,21 @@
+import './loading.css'
+const Loading = () => {
+    return (
+        <div className="loader loader--style2" title="1">
+            <svg  id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                width="60px" height="60px" >
+                <path fill="#ff5e03" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
+                    <animateTransform attributeType="xml"
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 25 25"
+                        to="360 25 25"
+                        dur="0.6s"
+                        repeatCount="indefinite" />
+                </path>
+            </svg>
+        </div>
+    );
+};
+
+export default Loading;
--- src/pages/view/view.component.jsx
+++ src/pages/view/view.component.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react';
 import ItemCard from './item-card/item-card.component';
+import Loading from '../../components/common/loading/loading.component';
 import './view.css';
 
 const getMenu = () => JSON.parse(localStorage.getItem('menu') || '[]');
 
 const ViewPage = (props) => {
     const [menu] = useState(getMenu);
+    const [loading, setLoading] = useState(true);
+
+    setTimeout(() => {
+        setLoading(false);
+    }, 1000);
+
+
     return (
         <div className='view-page'>
             <h1>view items</h1>
-            <div className="main">
-                {menu.map((item, index) => {
-                    return <ItemCard {...item} key={item + index}/>
-                })}
-            </div>
+            {loading
+                ? <Loading />
+                : <div className="main">
+                    {menu.map((item, index) => {
+                        return <ItemCard {...item} key={item + index} />;
+                    })}
+                </div>}
         </div>
     );
 };
