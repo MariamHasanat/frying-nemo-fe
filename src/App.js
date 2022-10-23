775b848603b30fee3576c194822fe02e4f8afdf5
sajeda <sajeda@gmail.com>
2022-10-23 12:58:29 +0200

    USE LINK IN URL &a

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ import AddPage from "./pages/add/add/add.component";
 
 import NotFound from "./pages/add/not-found/not-found";
 import View from "./pages/add/view/view";
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function App() {
-  const path = window.location.pathname;
-  console.log(path);
-  let page = null;
-  if (path === '/add')
-    page = <AddPage />
-  else if (path === '/view')
-    page = <View />
-  else page = <NotFound />
+  // const path = window.location.pathname;
+  // console.log(path);
+  // let page = null;
+  // if (path === '/add')
+  //   page = <AddPage />
+  // else if (path === '/view')
+  //   page = <View />
+  // else page = <NotFound />
 
 
   return (
     <div>
-      <Handel />
-      {page}
+
+      <BrowserRouter>
+        <Handel />
+        <Routes>
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/add" element={<AddPage />} />
+          <Route path="/view" element={<View />} />
+        </Routes>
+      </BrowserRouter>,
     </div>
   );
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,21 @@
 import './header.css'
+import { Link } from 'react-router-dom';
 const Handel =(props)=>{
   return(
   <header className='handel-group'>
     <img className='img' src ='./fish.png' />
     <h1>Seafood Restaurant</h1>
+    
     <div>
    
-        <div>
+        {/* <div>
           <a href="/add" >Add</a>
           </div>
-        <div>< a href = "/view">View</a></div>
+        <div>< a href = "/view">View</a></div> */}
+         <div>
+          <Link to="/add" >Add</Link>
+          </div>
+        <div>< Link to  = "/view">View</Link ></div>
      
     </div>
   </header>
