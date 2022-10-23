b955d86211dad5f34079bdc17d7a3555e5fb3ce1
Manolya <201097@ppu.edu.ps>
2022-10-23 11:32:06 +0200

    showing components depending on pathname

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ import './index.css';
 
 function App() {
     const [currentPage, setCurrentPage] = useState('404');
+
+    let pathname = window.location.pathname;
+    let page = null;
+    if (pathname === '/add')
+        page = <AddPage />;
+
+    else if (pathname === '/view')
+        page = <ViewPage />;
+
+    else
+        page = <NotFound />;
+
     return (
         <div>
             <Header img='images/nemo.png' width={100} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-            {currentPage === 'add' && <AddPage setCurrentPage={setCurrentPage}/>}
+            {page}
+            {/* {currentPage === 'add' && <AddPage setCurrentPage={setCurrentPage}/>}
             {currentPage === 'view' &&<ViewPage/>}
-            {currentPage === '404' && <NotFound/>}
+            {currentPage === '404' && <NotFound/>} */}
         </div>
     );
 }
