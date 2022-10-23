086e1cebd8c681ac6eb4f332cb38103bcabe3dbe
nadeenj <191080@ppu.edu.ps>
2022-10-23 12:16:21 +0200

    add o'clock

--- src/common/view/view.component.jsx
+++ src/common/view/view.component.jsx
@@ -1,12 +1,30 @@
 import './view.css';
 import Items from '../view/item/item.component';
-import { useState } from "react";
+import { useState,useEffect } from "react";
 const getMenu = ()=>JSON.parse(localStorage.menuItems|| '[]');
 const ViewPage = (props) => {
   /**
    * @type {{Array,Function}}
    * 
    */
+   const [loading, setLoading] = useState(false);
+   
+   const getMenuItems = () => {
+    setLoading(true);
+     // Run the code inside after 1000 milliseconds (1 Second)
+     setTimeout(() => {
+      const items = JSON.parse(localStorage.menuItems || '[]');
+
+      getMenuItems(items);
+      setLoading(false);
+    },1000);
+  };
+
+  useEffect(() => {
+    getMenuItems();
+  }, []);
+
+
   const[menuItems]=useState(getMenu());
   return (
     <div className="view-page">
