6e59c44c14611a153e378bf6549c2ce62b83453b
nadeenj <191080@ppu.edu.ps>
2022-10-27 12:52:34 +0200

    input search

--- src/common/view/view.component.jsx
+++ src/common/view/view.component.jsx
@@ -1,37 +1,59 @@
 import './view.css';
 import Items from '../view/item/item.component';
-import { useState,useEffect } from "react";
-const getMenu = ()=>JSON.parse(localStorage.menuItems|| '[]');
-const ViewPage = (props) => {
+import Input from '../input/input.component';
+import { useState, useEffect } from "react";
+const getMenu = () => JSON.parse(localStorage.menuItems || '[]');
+const ViewPage = () => {
   /**
-   * @type {{Array,Function}}
-   * 
+ * @type {Array<{
+ * name: string;
+   * description: string;
+   * ingredients: string[];
+   * price: number;
+   * category: string;
+   * image: string;
+   * }>}
    */
-   const [loading, setLoading] = useState(false);
-   
-   const getMenuItems = () => {
+  const initialItems = [];
+  const [loading, setLoading] = useState(false);
+  const [menuItems, setMenuItems] = useState(initialItems);
+  const [search, setSearch] = useState('');
+
+  const getMenuItems = () => {
     setLoading(true);
-     // Run the code inside after 1000 milliseconds (1 Second)
-     setTimeout(() => {
+    // Run the code inside after 1000 milliseconds (1 Second)
+    setTimeout(() => {
       const items = JSON.parse(localStorage.menuItems || '[]');
 
-      getMenuItems(items);
+      setMenuItems(items);
       setLoading(false);
-    },1000);
+    }, 1000);
   };
 
   useEffect(() => {
     getMenuItems();
   }, []);
-
-
-  const[menuItems]=useState(getMenu());
+  const filterItem = menuItems.filter(item => {
+    /**
+     * @param {string}str
+     */
+    const doesItemMatch = str=> str.toLowerCase().includes(search.toLowerCase().trim());
+    const match =(
+    doesItemMatch(item.name)||
+    doesItemMatch(item.description)||
+    item.ingredients.some(ingredient=>doesItemMatch(ingredient))
+);
+    return match;
+  });
+console.debug('items', menuItems, 'filtered', filterItem)
   return (
     <div className="view-page">
       <h1>View All Menu Items</h1>
-      <div className="item">
+      <Input type="search" placeholder='Search'value={search} onChange={e => setSearch(e.target.value)} />
+      <div className="item" >
         {
-          menuItems.map((item,index )=> <Items data={item} key={item.name + index}/>)
+          filterItem
+          .map((item, index) => <Items data={item} key={item.name + index} />)
         }
       </div>
     </div>
