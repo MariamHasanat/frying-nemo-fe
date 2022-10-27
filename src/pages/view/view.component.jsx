33ce45f86301fbaa2271509cb3ff9dffc5200333
Manolya <201097@ppu.edu.ps>
2022-10-27 12:57:39 +0200

    adding a search bar (searches for name, description, and ingredients)

--- src/pages/view/view.component.jsx
+++ src/pages/view/view.component.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import ItemCard from './item-card/item-card.component';
 import Loading from '../../components/common/loading/loading.component';
+import Input from '../../components/common/input/input.component';
 import './view.css';
 
 const getMenu = () => JSON.parse(localStorage.getItem('menu') || '[]');
@@ -8,24 +9,38 @@ const getMenu = () => JSON.parse(localStorage.getItem('menu') || '[]');
 const ViewPage = (props) => {
     const [menu] = useState(getMenu);
     const [loading, setLoading] = useState(true);
+    const [searchText, setSearchText] = useState('');
+    const filteredMenu = menu
+        .filter(
+            item => {
+                    return (item.name.toLowerCase().includes(searchText.trim().toLowerCase()))
+                    || (item.description.toLowerCase().includes(searchText.trim().toLowerCase()))
+                    || (item.ingredients.some(element =>  element.toLowerCase().includes(searchText.trim().toLowerCase())))
+                });
 
-    setTimeout(() => {
-        setLoading(false);
-    }, 1000);
+                setTimeout(() => {
+                    setLoading(false);
+                }, 1000);
 
 
-    return (
-        <div className='view-page'>
-            <h1>view items</h1>
-            {loading
-                ? <Loading />
-                : <div className="main">
-                    {menu.map((item, index) => {
-                        return <ItemCard {...item} key={item + index} />;
-                    })}
-                </div>}
-        </div>
-    );
-};
+                return (
+                    <div className='view-page'>
+                        <h1>view items</h1>
+                        <Input
+                            placeholder='Search'
+                            type="search" value={searchText}
+                            onChange={e => setSearchText(e.target.value)}
+                        />
 
-export default ViewPage;
+                        {loading
+                            ? <Loading />
+                            : <div className="main">
+                                {filteredMenu.map((item, index) => {
+                                    return <ItemCard {...item} key={item + index} />;
+                                })}
+                            </div>}
+                    </div>
+                );
+            };
+
+    export default ViewPage;
