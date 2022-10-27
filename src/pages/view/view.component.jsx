9d1c4bd26630a5a73d3859f7c6cc5c02bb7f26fc
yarasarahneh <yarasa2211@gmail.com>
2022-10-27 12:48:44 +0200

    add filter for ing and name

--- src/pages/view/view.component.jsx
+++ src/pages/view/view.component.jsx
@@ -4,32 +4,57 @@ import Item from './item/item.component';
 import Input from '../../components/common/input/input.component';
 
 const getMenuItems = () => JSON.parse(localStorage.MenuItems || '[]');
-
+/**
+ * @type {Array<{
+ * name: string;
+ * description: string;
+ * ingredients: string[];
+ * price: number;
+ * category: string;
+ * image: string;
+ * }>}
+ */
+const initialItems = [];
 
 const ViewPage = (props) => {
   /**
    * @type {{Array,Function}} loading
    */
-//   useEffect(()=>{
-// alert("welcome");
-//   },[]);
-  const  [menuItems ]= useState(getMenuItems());
-  const [searchTerms,setsearchTerms]=useState("");
-  const filterdItems = menuItems.filter(item =>{ return item.name.toLowerCase().includes(searchTerms.toLowerCase().trim())});
+  //   useEffect(()=>{
+  // alert("welcome");
+  //   },[]);
+  
+  const [menuItems, setMenuItems] = useState(initialItems);
+  const [searchTerms, setsearchTerms] = useState("");
+  const filterdItems =
+    menuItems.filter(item => {
+  /**
+  * 
+  * @param {string }  str
+  */
+      const DoesMatch = str=>str.toLowerCase().includes(searchTerms.toLowerCase().trim());
+
+      const match =(
+      DoesMatch(item.name || 
+        DoesMatch(item.description)||
+        item.ingredients.some(ingredients=>DoesMatch))
+      );
+    
+    });
   return (
     <div className="view-page">
       <h1> view menu items </h1>
 
       <Input type='search'
-      value={searchTerms}
-      placeholder="search"
-      onChange={e=> setsearchTerms(e.target.value)}
+        value={searchTerms}
+        placeholder="search"
+        onChange={e => setsearchTerms(e.target.value)}
       />
 
 
       <div className='item-container'>
         {
-        filterdItems.map((item ,index)=> <Item data={item} key={item.name}/>)
+         menuItems.map((item, index) => <Item data={item} key={item.name} />)
         }
 
       </div>
