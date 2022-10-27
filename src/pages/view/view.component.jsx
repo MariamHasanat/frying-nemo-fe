8979e2ced4750d81dd5c546980decfc23d85aff7
qjabari <qjabari@sadasol.com>
2022-10-27 13:00:39 +0200

    Filtering lists

--- src/pages/view/view.component.jsx
+++ src/pages/view/view.component.jsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from 'react';
+import Input from '../../components/common/input/input.component';
 import Spinner from '../../components/core/spinner/spinner.component';
 import Item from '../../components/view/item/item.component';
 import './view.css';
 
+/**
+ * @type {Array<{
+ * name: string;
+ * description: string;
+ * ingredients: string[];
+ * price: number;
+ * category: string;
+ * image: string;
+ * }>}
+ */
+const initialItems = [];
 
 const ViewPage = (props) => {
-  /**
-   * @type {[Array, Function]} Loading
-   */
-  const [menuItems, setMenuItems] = useState([]);
+  const [menuItems, setMenuItems] = useState(initialItems);
   const [loading, setLoading] = useState(false);
+  const [searchTerms, setSearchTerms] = useState('');
 
   const getMenuItems = () => {
     setLoading(true);
@@ -26,15 +36,37 @@ const ViewPage = (props) => {
     getMenuItems();
   }, []);
 
+  const filteredItems = menuItems.filter(item => {
+    /**
+     * Check if search terms are somewhere inside given string.
+     * @param {string} str 
+     */
+    const doesItMatch = str => str.toLowerCase().includes(searchTerms.toLowerCase().trim());
+
+    const match = (
+      doesItMatch(item.name) ||
+      doesItMatch(item.description) ||
+      item.ingredients.some(ingredient => doesItMatch(ingredient))
+    );
+
+    return match;
+  });
+
   return (
     <div className="view-page">
       <h1>View Menu Items</h1>
-
+      <Input
+        type="search"
+        value={searchTerms}
+        onChange={e => setSearchTerms(e.target.value)}
+        placeholder="Search"
+      />
       {loading
         ? <div style={{ display: 'flex', justifyContent: 'center' }}><Spinner /></div>
         : <div className="items-container">
           {
-            menuItems.map((item, index) => <Item data={item} key={item.name + index} />)
+            filteredItems
+              .map((item, index) => <Item data={item} key={item.name + index} />)
           }
         </div>
       }
