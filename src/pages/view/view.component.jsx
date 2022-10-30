838d3d721db313cadb31b9f73fb4ea8c66a7db18
ManolyaTam <tamimimanolya@gmail.com>
2022-10-30 13:10:07 +0200

    using useSearchParams (QUERY STRING)

--- src/pages/view/view.component.jsx
+++ src/pages/view/view.component.jsx
@@ -3,49 +3,54 @@ import ItemCard from './item-card/item-card.component';
 import Loading from '../../components/common/loading/loading.component';
 import Input from '../../components/common/input/input.component';
 import './view.css';
+import { useSearchParams } from 'react-router-dom';
 
 const getMenu = () => JSON.parse(localStorage.getItem('menu') || '[]');
 
 const ViewPage = (props) => {
     const [menu] = useState(getMenu);
     const [loading, setLoading] = useState(true);
-    const [searchText, setSearchText] = useState(localStorage.getItem('searchText-ViewPage') || '');
+    const [params, setParams] = useSearchParams();
 
-    const setSearchTextAndSaveToLocalStorage = (value) => {
-        setSearchText(value);
-        localStorage.setItem('searchText-ViewPage', value);
-    }
+    const searchTerms = params.get('searchTerms');
+    console.log('searchTerms:', searchTerms);
+
     const filteredMenu = menu
         .filter(
             item => {
-                    return (item.name.toLowerCase().includes(searchText.trim().toLowerCase()))
-                    || (item.description.toLowerCase().includes(searchText.trim().toLowerCase()))
-                    || (item.ingredients.some(element =>  element.toLowerCase().includes(searchText.trim().toLowerCase())))
-                });
+                return (item.name.toLowerCase().includes(searchTerms.trim().toLowerCase()))
+                    || (item.description.toLowerCase().includes(searchTerms.trim().toLowerCase()))
+                    || (item.ingredients.some(element => element.toLowerCase().includes(searchTerms.trim().toLowerCase())));
+            });
 
-                setTimeout(() => {
-                    setLoading(false);
-                }, 1000);
+    setTimeout(() => {
+        setLoading(false);
+    }, 1000);
 
 
-                return (
-                    <div className='view-page'>
-                        <h1>view items</h1>
-                        <Input
-                            placeholder='Search'
-                            type="search" value={searchText}
-                            onChange={e => setSearchTextAndSaveToLocalStorage(e.target.value)}
-                        />
+    return (
+        <div className='view-page'>
+            <h1>view items</h1>
+            <Input
+                placeholder='Search'
+                type="search" value={searchTerms}
+                onChange={e => {
+                    const newParams = new URLSearchParams(params);
+                    newParams.set('searchTerms', e.target.value);
 
-                        {loading
-                            ? <Loading />
-                            : <div className="main">
-                                {filteredMenu.map((item, index) => {
-                                    return <ItemCard {...item} key={item + index} />;
-                                })}
-                            </div>}
-                    </div>
-                );
-            };
+                    setParams(newParams);
+                }}
+            />
 
-    export default ViewPage;
+            {loading
+                ? <Loading />
+                : <div className="main">
+                    {filteredMenu.map((item, index) => {
+                        return <ItemCard {...item} key={item + index} />;
+                    })}
+                </div>}
+        </div>
+    );
+};
+
+export default ViewPage;
