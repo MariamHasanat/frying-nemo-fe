f62799424ca558455ac28dc9c35f84f7dc094159
Mariam <201011@ppu.ediu.ps>
2022-11-01 13:54:59 +0200

    create filter bar component

---
+++ src/pages/view/filter-bar/filter.bar.component.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Input from '../../../components/common/input';
+import { CATEGORIES } from '../../../components/data/categories';
+import Select from '../../../components/selector/selector.component';
+
+
+const Filter = (props) => {
+
+
+  const handleOnchangeOnURL = (filterName, inputValue) => {
+
+    const newParam = new URLSearchParams(props.params);
+    
+    if(inputValue) {
+      newParam.set(filterName, inputValue);
+    } else {
+      newParam.delete(filterName);
+    }
+    props.setParams(newParam);
+  };
+
+
+
+
+
+  return (
+    <div>
+
+      <Input
+        type="search"
+        value={props.searchParams}
+        onChange={e => {
+          handleOnchangeOnURL('q', e.target.value);
+        }}
+        placeholder="Search"
+      />
+      <Select
+        name='select'
+        label='select'
+        value={props.categoriesFromURL}
+        onChange={e => {
+          handleOnchangeOnURL('category', e.target.value)
+        }
+
+        }
+      >
+        {
+          CATEGORIES.map(item => {
+            return <option key={item} value={item}>{item}</option>;
+          }
+          )
+        }
+      </Select>
+    </div>
+
+  );
+};
+
+export default Filter;
