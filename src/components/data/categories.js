46c543546d5bc22af1ad26cb8ddb8e480f513950
Mariam <201011@ppu.ediu.ps>
2022-11-01 13:47:23 +0200

    create category file to reduce the code

---
+++ src/components/data/categories.js
@@ -0,0 +1,15 @@
+const CATEGORIES = [
+  
+    "tea",
+    "coffee",
+    "nemo",
+    "abu-nemo",
+    'Sandwiches',
+    'Main Dish',
+    'Appetizers',
+    'Ice Cream'
+]
+
+export {
+  CATEGORIES
+};
