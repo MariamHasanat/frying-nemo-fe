4933e66cb509f3b5f32e6d218792724a2ff8c719
Manolya <201097@ppu.edu.ps>
2022-11-03 13:00:45 +0200

    re-styling add and view links

--- src/components/core/header/header.component.jsx
+++ src/components/core/header/header.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './header.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 const Header = (props) => {
     const navigate = useNavigate();
+    const location = useLocation();
     return (
         <div>
             <div className='header'>
@@ -10,8 +11,8 @@ const Header = (props) => {
                 <img src='images/nemo.png' alt='logo' width={100} onClick={() => navigate('/')} />  {/* go to home page when clicking on logo */}
                 {props.text ? <span>{props.text}</span> : null}
                 <span>
-                    <Link to="/add" className='nemo-button'>add</Link>
-                    <Link to="/view" className='nemo-button'>view</Link>
+                    <Link to="/add" className={location.pathname === '/add'? 'active nemo-button': 'nemo-button'}>add</Link>
+                    <Link to="/view" className={location.pathname === '/view'? 'active nemo-button': 'nemo-button'}>view</Link>
                 </span>
             </div>
             <hr />
