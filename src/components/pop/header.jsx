fbb3a1f4c78cfced79e39236f14ef94cc62a5869
ayat <ayatabualln@gmail.com>
2022-11-03 13:56:49 +0200

    add some unknown changes :)))

--- src/components/pop/header.jsx
+++ src/components/pop/header.jsx
@@ -1,29 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './header.css';
-const Head =(props)=>{
 
+const Head = (props) => {
+  const location = useLocation();
 
-return(
+  return (
 
-<header>
-<div className="head-container">
-  <h1>
-<img src="./mylogo.svg" alt="HACONA MATATA" />
-HAKONA MATATA
-</h1>
-</div>
-<div className="right">
-        <nav style={{  margin: "1rem" }}>
+    <header>
+      <div className="head-container">
+        <h1>
+          <img src="./mylogo.svg" alt="HACONA MATATA" />
+          HAKONA MATATA
+        </h1>
+      </div>
+      <div className="right">
+        <nav style={{ margin: "1rem" }}>
           {/* <button className={props.currentPage === 'add' ? 'current' : ''} onClick={() => props.onNavigate('add')}>Add</button>
           <button className={props.currentPage === 'view' ? 'current' : ''} onClick={() => props.onNavigate('view')}>View</button> */}
-          <Link style={{padding:"1rem"}} to="./add">ADD</Link>
+
+          {/* location.pathname==='add'? "class 1 " : "class 2"  to add style to the nav links  */}
+
+          <Link style={{ padding: "1rem" }} to="./add">ADD</Link>
           <Link to="./view">VIEW</Link>
         </nav>
       </div>
 
-</header>
+    </header>
 
-);
+  );
 
-}
+};
 export default Head;
---
+++ src/pages/veiw-item/view-item.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+
+/**
+ * @type {Array<{
+ * name: string;
+ * description: string;
+ * ingredients: string[];
+ * price: number;
+ * category: string;
+ * image: string;
+ * }>}
+ */
+const ViewItemPage = (props) => {
+
+
+  return (
+    <div></div>
+  );
+
+
+};
+export default ViewItemPage;
+
+
