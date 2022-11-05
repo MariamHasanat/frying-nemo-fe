133eac3bc698d255b8a44eb25ca7cac746ff065b
Rami Ikhmayyes <191003@ppu.edu.ps>
2022-11-05 20:34:43 +0200

    card css & View page input

--- src/pages/view/ViewPage.component.jsx
+++ src/pages/view/ViewPage.component.jsx
@@ -1,9 +1,11 @@
 import "./ViewPage.css";
 import Cards from "../../components/card/card.component";
+import Input from "../../components/common/input/input.component";
 const ViewPage = (props) => {
   return (
     <div>
       <h1>view Page</h1>
+      <Input />
       <Cards />
     </div>
   );
