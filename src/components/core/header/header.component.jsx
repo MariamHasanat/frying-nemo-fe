65174bd7b7fe3572d0583c5108a63af3923465fe
Manolya <201097@ppu.edu.ps>
2022-11-06 12:05:34 +0200

    fixing nemo logo

--- src/components/core/header/header.component.jsx
+++ src/components/core/header/header.component.jsx
@@ -1,4 +1,5 @@
 import './header.css';
+import nemo from '../../../images/nemo.png'
 import { Link, useNavigate, useLocation, NavLink } from 'react-router-dom';
 const Header = (props) => {
     const navigate = useNavigate();
@@ -7,7 +8,7 @@ const Header = (props) => {
         <div>
             <div className='header'>
                 {/* <img src={props.img} alt='logo' width={props.width} /> */}
-                <img src='images/nemo.png' alt='logo' width={100} onClick={() => navigate('/')} />  {/* go to home page when clicking on logo */}
+                <img src={nemo} alt='logo' width={100} onClick={() => navigate('/')} />  {/* go to home page when clicking on logo */}
                 {props.text ? <span>{props.text}</span> : null}
                 <span>
                     <Link to="/add" className={location.pathname === '/add'? 'active nemo-button': 'nemo-button'}>add</Link>
