62b19f4afed5e40e7346262a8bb73888dde7fba4
Lana <lanaqawasmy7@gmail.com>
2022-11-06 12:27:38 +0200

    add spinner and navigate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+
 import AddPage from "./pages/add/add.component";
 import Header from "./components/header/header.component";
 import ViewPage from "./pages/view/view.component";
+import ViewItemPage from "./pages/view-item/view-item.component";
 import NotFoundPage from "./pages/not-found/not-found.component";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 
 function App() {
@@ -19,9 +20,11 @@ function App() {
 
         <Routes>
 
-          <Route path="/*" element={<NotFoundPage />} />
+          <Route path="/" element={<Navigate to="/view" replace />} />
           <Route path="/add" element={<AddPage />} />
           <Route path="/view" element={<ViewPage />} />
+          <Route path="/view-details/:id" element={<ViewItemPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
 
         </Routes>
 
---
+++ src/pages/view-item/view-item.component.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import './view-item.css';
+import { getItem } from '../../services/items';
+import Spinner from '../../components/spinner/spinner.component';
+
+/**
+ * @type {Array<{
+ * id: number;
+ * name: string;
+ * description: string;
+ * ingredients: string[];
+ * price: number;
+ * category: string;
+ * image: string;
+ * }>}
+ */
+
+const ViewItemPage = () => {
+  const params = useParams();
+  const [currentItem, setCurrentItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const item = getItem(params.id);
+    setCurrentItem(item);
+    setLoading(false);
+  }, []);
+
+
+  return (
+    <div className="view-item-page">
+      {loading && <Spinner />}
+      {
+        !loading && currentItem !== null
+          ? <div className="item-details">
+            <h1>{currentItem.name}</h1>
+            <div className="img">
+              <img src={currentItem.image} alt="food" />
+            </div>
+            <div className="info">
+              <p><b>Item Description: </b> {currentItem.description}</p>
+              <p className="ingredients"><b>Ingredients:</b>
+              <br/>{currentItem.ingredients.join(", ")}</p>
+            </div>
+            <div className="price">
+              <span><b>Price: </b>${currentItem.price}</span>
+              <div className="add-cart">
+                <button>+</button>
+                <input type="number" max={500} />
+                <button>-</button>
+              </div>
+            </div>
+          </div>
+          : <span>Item Not Found!</span>
+      }
+    </div>
+  );
+};
+
+export default ViewItemPage;
