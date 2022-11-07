25f6eed9d0bab205560d943b0c3ba22494daf6dc
=sajeda <=swadsajwda@gmail.com>
2022-11-07 19:47:38 +0100

    update viewItem Page

--- src/pages/add/viewitempage/viewitempage.jsx
+++ src/pages/add/viewitempage/viewitempage.jsx
@@ -5,18 +5,30 @@ import { getItem } from '../../../components/add/form/data/items';
 import Item from '../../../components/item/items/item';
 import Spinner from '../../../components/spinner/spinner';
 import './viewitempage.css';
+/**
+ * @type {Array<{
+ * id: number;
+ * name: string;
+ * description: string;
+ * ingredients: string[];
+ * price: number;
+ * category: string;
+ * image: string;
+ * }>}
+ */
 const ViewItemPage = (props) => {
   const params = useParams();
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const [currentItem, setCurrentItem] = useState(null);
   const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     setLoading(true);
     const item = getItem(params.id);
     console.log(params.id);
-    if(item === null){//اذا دخل id غلط 
-      navigate("/404")
+    if (item === null) {//اذا دخل id غلط 
+      navigate("/404");
     }
     setCurrentItem(item);
     setLoading(false);
@@ -25,15 +37,38 @@ const ViewItemPage = (props) => {
 
   return (
     <div className="view-item-page">
-      <h1>View Menu Item</h1>
+
       {loading && <Spinner />}
       {
         !loading && currentItem !== null
-          ? <Item data={currentItem} />
+          ? <div className="item-details">
+
+            <h1>{currentItem.name}</h1>
+
+            <div className="img">
+              <img src={currentItem.image} alt="food" />
+            </div>
+            <div className="info">
+              <p><b>Item Description: </b> {currentItem.description}</p>
+              <p className="ingredients"><b>Ingredients:</b>
+                <br />{currentItem.ingredients.join(", ")}</p>
+
+            </div>
+            <div className="price">
+              <span><b>Price: </b>${currentItem.price}</span>
+              <div className="add-cart">
+                <button>+</button>
+                <input type="number" max={500} />
+                <button>-</button>
+              </div>
+            </div>
+          </div>
+
+
           : <span>Item Not Found!</span>
       }
     </div>
 
   );
 };
-export default ViewItemPage;
+export default ViewItemPage;;
