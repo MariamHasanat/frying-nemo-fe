b629b38f2595bb7c114cca34ef24237b485da0c2
Nadeen Jubeh <nadeenjubeh123@gmail.com>
2022-11-07 17:40:49 +0200

    add css for log in page

--- src/pages/add/log-in/login.component.jsx
+++ src/pages/add/log-in/login.component.jsx
@@ -1,4 +1,26 @@
+import"./login.css";
+import  Input  from "../../../common/input/input.component";
 const LoginPage=()=>{
-    return(<div></div>)
+    return(
+    
+      <div>
+
+        <div className="body"></div>
+        <div className="grad"></div>
+        <div className="header">
+          <h1>Welcom Back</h1>
+        </div>
+        <br></br>
+        <div className="login">
+          <Input type="email" placeholder="example@example.com" /><br></br>
+          <Input type="password" placeholder="password" /><br></br>
+          <Input type="button" value="Login" />
+        </div>
+
+      </div>
+
+
+
+    );
 }
 export default LoginPage;
