40d0b4b0eae4488a56e999dad235b942d8b0037c
Lana <lanaqawasmy7@gmail.com>
2022-11-08 12:15:00 +0200

    render login page

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,18 @@ import ViewPage from "./pages/view/view.component";
 import ViewItemPage from "./pages/view-item/view-item.component";
 import NotFoundPage from "./pages/not-found/not-found.component";
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from "./pages/login/login.component";
 
 
 function App() {
 
+  const initialUser = JSON.parse(sessionStorage.getItem('user'));
+  const [user, setUser] = useState(initialUser);
+
+  const setUserOverride = user => {
+    setUser(user);
+    sessionStorage.setItem('user', JSON.stringify(user));
+  };
 
   return (
 
@@ -16,11 +24,12 @@ function App() {
 
       <BrowserRouter>
 
-        <Header />
+        <Header  user={user} />
 
         <Routes>
 
           <Route path="/" element={<Navigate to="/view" replace />} />
+          <Route path="/login" element={<LoginPage user={user} setUser={setUserOverride} />} />
           <Route path="/add" element={<AddPage />} />
           <Route path="/view" element={<ViewPage />} />
           <Route path="/view-details/:id" element={<ViewItemPage />} />
