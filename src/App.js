43b3e8ff1ee59cc238d2e6ac544dc0644da19bbc
raghad__ns <raghadnadersalem2002017@GMAIL.COM>
2022-11-08 14:01:46 +0200

    react context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Header from "./components/common/header/header.component";
 import AddPage from "./pages/add/add.component";
 import ViewPage from "./pages/view/view.component";
-import { useState } from "react";
+import React , { useState } from "react";
 import './common.css' ;
 import NotFound from "./pages/not-found/notFound.component";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, RouterProvider, Routes } from "react-router-dom";
 import SingleItem from "./pages/view/cards/single-item/single-item.component";
 import LoginPage from "./pages/login/login.component";
+
+export const UserContext = React.createContext(null) ; // provider 
 function App() {
   const initialUser =  JSON.parse(sessionStorage.getItem ('user')) ;
   const [user , setUser] = useState (initialUser) ;
   const [items , setItems] = useState([]) ;
-
+  
   const setUserOverride = user => {
     setUser (user) ;
     sessionStorage.setItem ('user' , JSON.stringify (user)) ;
@@ -21,10 +23,10 @@ function App() {
     tempItems.push (item) ;
     setItems (tempItems) ;
   }
-
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter >
+      <UserContext.Provider value = {{user , setUser : setUserOverride}}>
         <Header 
           img = "./nemo.svg.svg" 
           title = "Frying Nemo" 
@@ -33,12 +35,13 @@ function App() {
         />
         <Routes>
           <Route path="/" element = {<Navigate to =  '/view' replace />} />   {/* page redirection using navigate component , which is built in react router dom library */}
-          <Route path="/add" element = {<AddPage onAdd = {addItem} user = {user} />} />
+          <Route path="/add" element = {<AddPage onAdd = {addItem} />} />
           <Route path="/login" element = {<LoginPage user = {user} setUser = {setUserOverride}/>}/>
           <Route path="/view" element = {<ViewPage user = {user}/>} />
           <Route path="/view-details/:id" element = {<SingleItem />} />
           <Route path="/*" element = {<NotFound  />} />
         </Routes> 
+      </UserContext.Provider>
       </BrowserRouter>
       {/* {page} */}
       {/* {currentPage === 'add' && 
