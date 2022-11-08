102abb56c6e43ca473cd205de8069e114122270d
Lana <lanaqawasmy7@gmail.com>
2022-11-08 12:11:20 +0200

    add user.js file

---
+++ src/services/users.js
@@ -0,0 +1,15 @@
+import { USERS } from '../data/tmp_data';
+
+/**
+ * Fake Fetching of single item
+ * @param {string} email
+ * @param {string} password
+ */
+const loginUser = (email, password) => {
+  const user = USERS.find(user => user.email === email && user.password === password);
+  return user || null;
+};
+
+export {
+  loginUser
+};
