b1911b109214c3b69c713beb748442b06b44b4de
Mariam <201011@ppu.ediu.ps>
2022-11-08 12:54:40 +0200

    initialize user , and fix the problem in the code

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ import ViewPage from "./pages/view/view.container";
 import NotFound from "./pages/not-found/not-found.component";
 import ViewItemPage from "./pages/view-item/view.item";
 import LoginPage from "./pages/login/login";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 //A function App will render the components
 function App() {
+  const initialUser = JSON.parse(sessionStorage.getItem('user'));
+
+  const [user , setUser] = useState(initialUser);
+
+  const setUserOverride = user => {
+    setUser(user);
+    sessionStorage.setItem('user', JSON.stringify(user));
+  }
 
   return (
     <div className="pages-container">
@@ -18,9 +27,9 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Navigate to="/view" replace/>} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/add" element={<AddPage />} />
-            <Route path="/view" element={<ViewPage />} />
+            <Route path="/login" element={<LoginPage user={user} setUser={setUserOverride} />} />
+            <Route path="/add" element={<AddPage user={user} />} />
+            <Route path="/view" element={<ViewPage user={user} />} />
             <Route path="/view/:id" element={<ViewItemPage />} />
             <Route path="/*" element={<NotFound />} />
           </Routes>
