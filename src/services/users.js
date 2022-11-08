55261eae90df4778d9b2c0023c3c7700ef5a19a0
qjabari <qjabari@sadasol.com>
2022-11-08 12:03:29 +0200

    Improvements

--- src/services/users.js
+++ src/services/users.js
@@ -1,12 +1,13 @@
 import { USERS } from '../data/tmp_data';
+
 /**
  * Fake Fetching of single item
  * @param {string} email
  * @param {string} password
  */
 const loginUser = (email, password) => {
-  const user = USERS.filter(user => user.email === email && user.password === password);
-  return user[0] || null;
+  const user = USERS.find(user => user.email === email && user.password === password);
+  return user || null;
 };
 
 export {
