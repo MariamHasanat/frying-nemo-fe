86e7dd14ffbc34924a450e15c714805803a5d59d
Mariam <201011@ppu.ediu.ps>
2022-11-10 13:17:27 +0200

    return the add page as before

--- src/pages/menuPageContainer/page.container.jsx
+++ src/pages/menuPageContainer/page.container.jsx
@@ -1,15 +1,11 @@
 import './pageContainerStyle.css';
-
 import Form from '../../components/form/form.component';
-import WithBorder from '../../components/with-border/with-border';
 
 const AddPage = (props) => {
   return (
     <div className='form-container'>
       <h1 className='h1'>Add a new item</h1>
-      <WithBorder>
-        <Form />
-      </WithBorder>
+      <Form />
     </div>
   );
 };
