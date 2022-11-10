ed8668cb99b6e466a4e80793f0a8091962abd4e5
Mariam <201011@ppu.ediu.ps>
2022-11-10 13:53:35 +0200

    create guard component

---
+++ src/components/core/guard/guard.jsx
@@ -0,0 +1,14 @@
+import React, {useContext} from "react";
+import { UserContext } from "../../user-provider/user-provider";
+
+const Guard =(props)=>{
+  
+  const userContext = useContext(UserContext)
+  
+  return(
+
+
+  );
+}
+
+export default Guard;
