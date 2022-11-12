39c6367a8da405f0869622a1d000f61a8c5449b5
ManolyaTam <tamimimanolya@gmail.com>
2022-11-12 09:06:11 +0200

    adding card component

---
+++ src/components/common/card/card.component.jsx
@@ -0,0 +1,11 @@
+import './card.css';
+
+const Card = (props) => {
+    return (
+        <div className='myCard'>
+            {props.children}
+        </div>
+    )
+
+};
+export default Card;
--- src/components/item-card/item-card.component.jsx
+++ src/components/item-card/item-card.component.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './item-card.css';
 import { Link } from 'react-router-dom';
+import Card from '../common/card/card.component';
 
 /**
  * 
@@ -25,7 +26,7 @@ const ItemCard = (props) => {
     const [quantity, setQuantity] = useState(0);
     return (
         <div className='item-card'>
-            <div className="card-wrapper">
+            <Card>
                 <img src={props.img} width={350} height={230} alt={props.name} />
                 <div className="item-description">
                     <Link to={`/view/${props.id}`}>{props.name}</Link >
@@ -54,7 +55,7 @@ const ItemCard = (props) => {
 
                 </div>
 
-            </div>
+            </Card>
         </div>
     );
 };
