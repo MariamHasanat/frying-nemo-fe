90d3fec2e88e16b36431f9bf0178740424bdf444
ManolyaTam <tamimimanolya@gmail.com>
2022-11-12 07:51:10 +0200

    a small change

--- src/components/core/header/header.component.jsx
+++ src/components/core/header/header.component.jsx
@@ -13,7 +13,7 @@ const Header = (props) => {
             <div className='header'>
                 <img src={nemo} alt='logo' width={100} onClick={() => navigate('/')} />  {/* go to home page when clicking on logo */}
                 {props.text ? <span>{props.text}</span> : null}
-                <span style={{"margin-left": 100}}>
+                <span style={{"marginLeft": 100}}>
                     <Link to="/add" className={location.pathname === '/add' ? 'active nemo-button' : 'nemo-button'}>add</Link>
                     <NavLink to="/view" className={(location.pathname === '/view' || location.pathname === '/view/') ? 'active nemo-button' : 'nemo-button'}>view</NavLink>
                 </span>
