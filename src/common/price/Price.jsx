3b9974a73ad4c91400b06f61543eeea4fbada83a
dala_bilal <dala@gmail.com>
2022-11-22 13:21:00 +0200

    Fix a error With dummy function

--- src/common/price/Price.jsx
+++ src/common/price/Price.jsx
@@ -13,7 +13,7 @@ const Price = (props) => {
     <span>${props.item.price}</span>
     <div className="add-cart">
       <button onClick={handelIncrement}>+</button>
-      <input type="number" max={500} value={props.getCartQuntity}/>
+      <input type="number" max={500} value={props.getCartQuntity} disabled onChange={() => {}}/> {/* dummy function */}
       <button onClick={handelDecrement}>-</button>
     </div>
   </div>
