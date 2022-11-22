5b8117659b469a1ac54f3433d8895fdddc51f32a
Manolya <201097@ppu.edu.ps>
2022-11-22 11:29:34 +0200

    changing session storage to lacal storage to store user info

--- src/components/providers/user-provider.component.jsx
+++ src/components/providers/user-provider.component.jsx
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 
 export const UserContext = React.createContext(null);
 const UserProvider = (props) => {
-    const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('user')));
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
     const setUserOverride = user => {
         setUser(user);
-        sessionStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem('user', JSON.stringify(user));
     };
 
     return (
