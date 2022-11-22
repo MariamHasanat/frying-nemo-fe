e82b66ca39b6e7402b3d264e50c740e3fa6b092c
Momen-G-Ar-Official <cp.momen@gmail.com>
2022-11-22 16:53:20 +0200

    save the cart page from un loged in users

--- src/pages/cart/cart.page.jsx
+++ src/pages/cart/cart.page.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import './cart.css';
 
 import CartList from '../../components/cart/list/cart-list.component';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../../components/providers/user-provider.component';
+import { useEffect } from 'react';
 
 const Cart = () => {
+    const userContext = useContext(UserContext);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (userContext.user === null) {
+            navigate('/log-in', { replace: true });
+        }
+    }, []);
     return (
         <div className='cart-page'>
-            <h2>Cart</h2>
+            <h2 className='header-in-cart'>Cart</h2>
             <CartList />
         </div>
     );
