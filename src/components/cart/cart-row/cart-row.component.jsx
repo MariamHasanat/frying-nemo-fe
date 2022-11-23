49b1557019c76188d9c42d268b0b5a7c64f3079e
ManolyaTam <tamimimanolya@gmail.com>
2022-11-23 22:47:49 +0200

    adding delete button

--- src/components/cart/cart-row/cart-row.component.jsx
+++ src/components/cart/cart-row/cart-row.component.jsx
@@ -1,7 +1,14 @@
 import PlusMinusButtons from '../../plus-minus-buttons/plus-minus-buttons.component';
+import trash from '../../../images/trash.png';
+import { useContext } from 'react';
+import { CartContext } from '../../providers/cart-provider.component';
 import './cart-row.css';
 
 const CartRow = (props) => {
+    const cartContext = useContext(CartContext);
+    const deleteItem = () => {
+        cartContext.dispatch({ type: 'DELETE_CART_ITEM', meal: props.item.meal });
+    };
     return (
         <li className='cart-row'>
             <div className="left">
@@ -10,7 +17,14 @@ const CartRow = (props) => {
             <div className="center">
                 <h2>{props.item.meal.name}</h2>
                 <p>${props.item.meal.price}&nbsp;&nbsp;<span className='green-text'>In Stock</span></p>
-                <PlusMinusButtons item={props.item.meal} quantity={props.item.quantity}/>
+                <PlusMinusButtons item={props.item.meal} quantity={props.item.quantity} />
+                {/* i should later get this div out and style it */}
+                <div className='right'>
+                    <button onClick={deleteItem}>
+                        <img src={trash} width={25} alt="" />
+                    </button>
+                    
+                </div>
             </div>
         </li>
     );
