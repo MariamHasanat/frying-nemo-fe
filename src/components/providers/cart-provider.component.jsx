26e349316a7c41532212ca8f20fac50ec82ce799
ManolyaTam <tamimimanolya@gmail.com>
2022-11-23 22:57:05 +0200

    adding cart to local storage, and removing it on logout

--- src/components/providers/cart-provider.component.jsx
+++ src/components/providers/cart-provider.component.jsx
@@ -1,11 +1,14 @@
 
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { reducer } from '../../reducers/cart';
 
 export const CartContext = React.createContext(null);
 
 const CartProvider = (props) => {
-    const [cart, dispatch] = useReducer(reducer, []);
+    const cartFromLocalStorage = JSON.parse(localStorage.getItem('cart') || '[]');
+    const [cart, dispatch] = useReducer(reducer, cartFromLocalStorage);
+
+    useEffect(() => localStorage.setItem('cart', JSON.stringify(cart)), [cart]);
 
     return (
         <CartContext.Provider value={{ cart, dispatch }}>
--- src/components/providers/user-provider.component.jsx
+++ src/components/providers/user-provider.component.jsx
@@ -6,8 +6,16 @@ const UserProvider = (props) => {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
     const setUserOverride = user => {
         setUser(user);
-        localStorage.setItem('user', JSON.stringify(user));
+
+        if (user === null) {
+            localStorage.removeItem('user');
+            localStorage.removeItem('cart');
+        }
+        else {
+            localStorage.setItem('user', JSON.stringify(user));
+        }
     };
+
 
     return (
         <UserContext.Provider value={{ user, setUser: setUserOverride }}>
