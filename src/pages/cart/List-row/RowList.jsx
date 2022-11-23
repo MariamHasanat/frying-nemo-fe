fa3d4569fab2d261c7f7ef819a86b47fd9034b60
dala_bilal <dalahhashlamoon@gmail.com>
2022-11-24 00:16:56 +0200

    display the total price

--- src/pages/cart/List-row/RowList.jsx
+++ src/pages/cart/List-row/RowList.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './rowlist.css'
+import { useContext } from 'react';
+import { CartContext } from '../../../components/add/form/provider/CartProvider';
 
 const RowList = (props) => {
   const { meal , quantity} = props.cartItem;
+  const cartContext = useContext(CartContext);
+  let totalPrice = 0;
+   for (let i = 0; i < cartContext.cart.length; i++) {
+    totalPrice += (cartContext.cart[i].quantity * cartContext.cart[i].meal.price);
+   }
   const handelIncrement = () => {
     props.dispatch({ type: 'Increment', meal });
   }
@@ -30,6 +37,7 @@ const RowList = (props) => {
          <span onClick={handelDelete}>x</span>
          <h3>${meal.price*quantity}</h3>
       </div>
+       
     </div>
   )
 }
