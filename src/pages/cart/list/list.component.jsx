df5fa5d4208739e6d7de18e34839dafe452deeca
Lana <lanaqawasmy7@gmail.com>
2022-11-24 12:09:28 +0200

    add clear all button to cart and compute total

--- src/pages/cart/list/list.component.jsx
+++ src/pages/cart/list/list.component.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
+import './list.css';
 import { useContext } from 'react';
 import { CartContext } from '../../../components/providers/cart-provider.component';
 import CartRow from '../row/row.component';
 
+
+
 const CartList = (props) => {
   const cartContext = useContext(CartContext);
+
+  const clear = () => cartContext.dispatch({ type: 'CLEAR' });
+
+  if (cartContext.cart.length === 0) {
+    return (
+      <div className="no-results">
+        <img src="./empty_cart.webp" alt="empty cart" width={300} />
+        <p>Your Cart is Empty!</p>
+      </div>
+    );
+
+  }
+
+  let total = 0;
+  for (const item of cartContext.cart) {
+    total += item.quantity * item.meal.price;
+  };
+
   return (
-    cartContext.cart.length
-      ? <ul className="cart-list">
+    <>
+      <ul className="cart-list">
         {
           cartContext.cart.map((item, index) => <CartRow item={item} dispatch={cartContext.dispatch} key={"r_" + index} />)
         }
       </ul>
-      : <div className="no-results">
-        <img src="./empty_cart.webp" alt="empty cart" width={300} />
-        <p>Your Cart is Empty!</p>
+
+      <div className="cartBottomRow">
+        <button className="nemo-cart" onClick={clear}>Clear Cart</button>
+
+        <span>Total: ${total}</span>
       </div>
+    </>
   );
 };
 
