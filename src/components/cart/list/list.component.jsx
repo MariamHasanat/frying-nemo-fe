fba875d26f8da40ade50ba5507f3e09856b624f3
Mariam <201011@ppu.ediu.ps>
2022-11-24 11:52:15 +0200

    add a new row of clear all and calculate the total

--- src/components/cart/list/list.component.jsx
+++ src/components/cart/list/list.component.jsx
@@ -1,21 +1,44 @@
+import { getToPathname } from '@remix-run/router';
 import React from 'react';
 import { useContext } from 'react';
 import { CartContext } from '../../providers/cart-provider.jsx';
 import CartRow from '../row/row.component';
 
 const CartList = (props) => {
+
   const cartContext = useContext(CartContext);
+  const clear = () => cartContext.dispatch({ type: 'CLEAR' })
+
+
+  
+  let total = 0;
+
+  for (const item of cartContext.cart) {
+    total += item.quantity * item.meal.price;
+  }
+
+
+  if (cartContext.cart.length === 0)
+    return (
+      <div className="no-results">
+        <img src="./empty_cart.webp" alt="empty cart" width={300} />
+        <p>Your Cart is Empty!</p>
+      </div>
+    );
+
   return (
-    cartContext.cart.length
-      ? <ul className="cart-list">
+
+    <>
+      <ul className="cart-list">
         {
           cartContext.cart.map((item, index) => <CartRow item={item} dispatch={cartContext.dispatch} key={"r_" + index} />)
         }
       </ul>
-      : <div className="no-results">
-        <img src="./empty_cart.webp" alt="empty cart" width={300} />
-        <p>Your Cart is Empty!</p>
+      <div>
+        <button className='nemo-button grey' onClick={clear}>Clear All</button>
+        <span>TOTAL : ${total}</span>
       </div>
+    </>
   );
 };
 
