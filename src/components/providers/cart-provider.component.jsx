0f57262bc858060043b930c0bf896368eed25294
raghad__ns <raghadnadersalem2002017@GMAIL.COM>
2022-11-24 12:54:29 +0200

    store cart in local storage as a map with user email as key

--- src/components/providers/cart-provider.component.jsx
+++ src/components/providers/cart-provider.component.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useContext } from 'react';
+import { useEffect } from 'react';
 import { useReducer } from 'react';
-import { initialState, reducer } from '../reducers/cart';
+import { reducer } from '../reducers/cart';
+import { UserContext } from './user-provider.component';
 export const CartContext = React.createContext (null) ;
 const CartProvider = (props) => {
-  const [cart , dispatch] = useReducer (reducer , initialState) ;
+  const userContext = useContext (UserContext) ;
+
+  const cartMap = JSON.parse (localStorage.getItem ("cartMap")) || {} ;
+  const cartKey = userContext.user?.email || "anonymous" ;
+  
+  const [cart , dispatch] = useReducer (reducer , cartMap[cartKey] || []) ;
+
+  useEffect (()=> {
+    const map = JSON.parse (localStorage.getItem ("cartMap")) || {} ;
+    const key = userContext.user?.email || "anonymous" ;
+    map[key] = cart ;
+    localStorage.setItem ("cartMap" , JSON.stringify (map));
+  } , [cart])
+
   return (
     <CartContext.Provider value = {{cart , dispatch}}>
       {props.children}
