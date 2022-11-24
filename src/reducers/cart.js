4f04e0b8b49a777d1f0238eba3b82d31e88c2382
Lana <lanaqawasmy7@gmail.com>
2022-11-24 07:36:05 +0200

    Preserve cart value through page refresh (localStorage)

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,4 +1,4 @@
-const initialState = [];
+
 
 const reducer = (cart, action) => {
   // This function updates the state
@@ -43,6 +43,5 @@ const reducer = (cart, action) => {
 };
 
 export {
-  reducer,
-  initialState
+  reducer
 };
