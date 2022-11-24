89da7beee52fc0bc2ec32fbd4ea0d595941b657e
Momrn-G-AR <cp.momen@gmail.com>
2022-11-24 12:07:06 +0200

    edit style of clear & total

--- src/pages/cart/cart.page.jsx
+++ src/pages/cart/cart.page.jsx
@@ -9,16 +9,6 @@ import { CartContext } from '../../components/providers/cart-provider.component'
 
 import trash from '../../assets/images/empty_cart.webp';
 
-/**
- * @param {{
- *      cart:Array<{
- *          item:{
- *                             
- *          }
- *          quantity:number;
- *      }
- * }} cartContext
- */
 const Cart = () => {
     const cartContext = useContext(CartContext);
     const userContext = useContext(UserContext);
@@ -28,7 +18,16 @@ const Cart = () => {
             navigate('/log-in', { replace: true });
         }
     }, []);
-
+    if (cartContext.cart.length === 0) {
+        return (
+            <div className='empty-cart-page'>
+                <img src={trash} alt='Empty Cart' />
+                <h2 className='header-in-empty-cart'>
+                    The cart is empty
+                </h2>
+            </div>
+        );
+    }
     const handleClearAll = () => {
         cartContext.dispatch({ type: 'CLEAR_ALL' });
     };
@@ -36,35 +35,27 @@ const Cart = () => {
     const totalPrice = cartContext.cart.reduce((val, cartItem) => {
         return val + (cartItem.quantity * cartItem.item.price);
     }, 0);
-   
+
     return (
         <div>
-            {
-                cartContext.cart.length
-                    ? <div className='cart-page'>
-                        <div className='header-in-cart'>
-                            <span>Cart</span>
-                            <div className='price-and-clear'>
-                                <span>
-                                    Total Price: ${totalPrice} 
-                                </span>
-                                <button
-                                    className='nemo-button'
-                                    onClick={handleClearAll}
-                                >
-                                    Clear All
-                                </button>
-                            </div>
-                        </div>
-                        <CartList />
-                    </div>
-                    : <div className='empty-cart-page'>
-                        <img src={trash} alt='Empty Cart' />
-                        <h2 className='header-in-empty-cart'>
-                            The cart is empty
-                        </h2>
-                    </div>
-            }
+            <div className='cart-page'>
+                <div className='header-in-cart'>
+                    <span>Cart</span>
+                </div>
+                <CartList />
+                <div className='price-and-clear'>
+                    <button
+                        className='nemo-button'
+                        onClick={handleClearAll}
+                    >
+                        Clear All
+                    </button>
+                    <span>
+                        Total Price: ${totalPrice}
+                    </span>
+                </div>
+            </div>
+
         </div >
     );
 };
