77bdeb9aae01cf72979e434f1e3709d9ececbe5a
Mariam <201011@ppu.ediu.ps>
2022-11-24 13:47:41 +0200

    cart provider code

--- src/components/providers/cart-provider.jsx
+++ src/components/providers/cart-provider.jsx
@@ -2,12 +2,43 @@
 import React from "react";
 import { useReducer } from "react";
 import { initialState, reducer } from '../../reducer/cart';
+import { UserContext } from "../../providers/user-provider";
+
+/********************************* */
 
 export const CartContext = React.createContext(null);
-
+​
+const getCartFromLocalStorage = email => {
+  const map = JSON.parse(localStorage.getItem('cartMap') || '{}');
+  const key = email || 'anonymous';
+  const cart = map[key] || [];
+  return cart;
+};
+​
+const updateCartInLocalStorage = (email, cart) => {
+  const map = JSON.parse(localStorage.getItem('cartMap') || '{}');
+  const key = email || 'anonymous';
+  map[key] = cart;
+  localStorage.setItem('cartMap', JSON.stringify(map));
+};
+​
+/**
+ * @param {{
+ *  children: React.ReactNode;
+ * }} props Component props
+ */
 const CartProvider = (props) => {
+  const userContext = useContext(UserContext);
+  const user = userContext.user;
+  const cartFromLocalStorage = getCartFromLocalStorage(user?.email);
+​
+  const [cart, dispatch] = useReducer(reducer, cartFromLocalStorage);
+​
+  useEffect(() => updateCartInLocalStorage(user?.email, cart), [cart]);
+​
+  useEffect(() => dispatch({ type: 'SET', cart: getCartFromLocalStorage(user?.email) }), [user]);
 
-  const [cart, dispatch] = useReducer(reducer, initialState);
+/***************************** */
 
   return (
    
