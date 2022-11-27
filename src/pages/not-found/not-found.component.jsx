c5f3f8f565e7b2d5bb1b2437bd2c1bd1ea1c5576
Mariam Hasanat <201011@ppu.edu.ps>
2022-11-27 10:01:41 +0200

    changes on not found page , i didn't import the illustration folder  in the not found page

--- src/pages/not-found/not-found.component.jsx
+++ src/pages/not-found/not-found.component.jsx
@@ -1,28 +1,9 @@
-import { useEffect, useState } from 'react';
 import './not-found.css';
-
 const NotFound = (props) => {
-
-  const [counter, setCounter] = useState(0);
-  const [win, setWinner] = useState(false);
-  useEffect(() => {
-    if(counter >= 10){  
-      setWinner(true);
-    }
-   }, [counter]);
-
 
   return (
     <div className="not-found-page">
-      <h1>404 Page Not Found :'( </h1>
-      <div>{counter}</div>
-      <button type='button' onClick={
-        () => setCounter(counter + 1)
-      }>+1</button>
-
-      {
-        win && <div>you are the winner :D</div>
-      }
+      not found page 
 
     </div>
   );
