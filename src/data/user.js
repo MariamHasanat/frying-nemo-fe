d50542fa2b39991199784e697f04fa7d460396c9
Nadeen Jubeh <nadeenjubeh123@gmail.com>
2022-12-03 00:02:39 +0200

    Fetch user from API

--- src/data/user.js
+++ src/data/user.js
@@ -1,14 +1,24 @@
-import { USERS } from './temp_data';
 /**
  * Fake Fetching of single item
  * @param {string} email
  * @param {string} password
  */
 const loginUser = (email, password) => {
-  const user = USERS.filter(user => user.email === email && user.password === password);
-  return user[0] || null;
-};
+  return fetch("https://6385ec80beaa6458266d44f1.mockapi.io/nemo/users")
+    .then(async (response) => {
+      const users = await response.json();
+      console.log(users);
+      const result = await users.filter(user =>
+        user['email'] === email && user['password'] === password); console.log(users['email']);
+      return result;
 
+    }).then((user) => {
+      return user[0];
+    })
+    .catch((error) => {
+      alert(error.toString());
+    });
+};
 export {
   loginUser
 };
--- src/pages/add/log-in/login.component.jsx
+++ src/pages/add/log-in/login.component.jsx
@@ -1,59 +1,52 @@
-import"./login.css";
-import  Input  from "../../../common/input/input.component";
+import "./login.css";
+import Input from "../../../common/input/input.component";
 import { useNavigate } from 'react-router-dom';
-import React,{ useEffect,useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { loginUser } from "../../../data/user";
 import { UserContext } from "../../../components/provider/provider.component";
-import Header from "../../../common/header/header.component";
 
-const LoginPage=()=>{
-  const navigate=useNavigate();
-  const userContext=useContext(UserContext);
+const LoginPage = () => {
+  const navigate = useNavigate();
+  const userContext = useContext(UserContext);
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        const email = e.target.email.value.trim();
-        const password = e.target.password.value.trim();
-
-        if (email && password) {
-          const user = loginUser(email,password);
-          if (user) {
-            userContext.setUser(user);
-            navigate('/view', { replace: true });
-          } else {
-            alert("Email or Password are not correct! Please try again.");
-          }
-        }
-      };
- useEffect(() => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value.trim();
+    const password = e.target.password.value.trim();
+    console.log(email);
+    console.log(password);
+    const user = await loginUser(email, password);
+    console.log(user);
+    if (user) {
+      userContext.setUser(user);
+      navigate('/view', { replace: true });
+    } else {
+      alert('email or password are not correct, try again !');
+    }
+  };
+  useEffect(() => {
     // To check if the user is already logged in, send him to the view page
     if (userContext.user?.id) {
       navigate('/view', { replace: true });
     }
   }, []);
 
-    return(
-    
-      <div className="jhk">
-        <div className="body"></div>
-        <div className="grad"></div>
-        <div className="header">
-          <h1>Welcome Back</h1>
-        </div>
-        <br></br>
-        <div className="login">
-          <form onSubmit={handleLogin}>
-           <Input type="email" placeholder="example@example.com" name="email" /><br></br>
-          <Input type="password" placeholder="password"  name="password"/><br></br>
-          <input type="submit" value={"Login"} />
-          </form>
-          
-        </div>
-
+  return (
+    <div className="jhk">
+      <div className="body"></div>
+      <div className="grad"></div>
+      <div className="header">
+        <h1>Welcome Back</h1>
       </div>
-
-
-
-    );
-}
+      <br></br>
+      <div className="login">
+        <form onSubmit={handleLogin}>
+          <Input type="email" placeholder="example@example.com" name="email" /><br></br>
+          <Input type="password" placeholder="password" name="password" /><br></br>
+          <input type="submit" value={"Login"} />
+        </form>
+      </div>
+    </div>
+  );
+};
 export default LoginPage;
