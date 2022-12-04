4cb2e8747cf9eb2d325f61094f59fc3dd36aa0c0
arwwd2002 <arwwd2002@gmail.com>
2022-12-04 11:09:01 +0200

    User API

--- src/services/users.js
+++ src/services/users.js
@@ -1,12 +1,24 @@
-import { USERS } from '../data/tmp_data';
+import { getUsers } from '../data/tmp_data';
 /**
  * Fake Fetching of single item
  * @param {string} email
  * @param {string} password
  */
-const loginUser = (email, password) => {
-  const user = USERS.find(user => user.email === email && user.password === password);
-  return user || null;
+// const loginUser = (email, password) => {
+//   const user = USERS.find(user => user.email === email && user.password === password);
+//   return user || null;
+// };
+
+// export {
+//   loginUser
+// };
+
+const loginUser = async (email, password) => {
+  const USER = await getUsers();
+  console.log(USER);
+  const user = USER.filter(user => user.email === email && user.password === password);
+  console.log(user[0]);
+  return user[0] || null;
 };
 
 export {
