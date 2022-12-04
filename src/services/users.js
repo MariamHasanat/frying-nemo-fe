4fe267bb4852bb5de6e5544bef335560ab566e10
moath rabai <191132@ppu.edu.ps>
2022-12-04 12:00:02 +0200

    integrate user API

--- src/services/users.js
+++ src/services/users.js
@@ -5,8 +5,13 @@ import { USERS } from '../data/tmp_data';
  * @param {string} password
  */
 const loginUser = (email, password) => {
-  const user = USERS.find(user => user.email === email && user.password === password);
-  return user;
+  return fetch("https://6385ec80beaa6458266d44f1.mockapi.io/nemo/users").then(async response => {
+    const users = await response.json();
+    return users.find(user => user.email === email && user.password === password) || null;
+  }).catch(error => {
+    console.log(error);
+    return null;
+  });
 };
 
 export {
