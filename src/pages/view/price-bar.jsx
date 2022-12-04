6e5958f22f8109a49abbae59e61e81e512835419
Ayat AbuAllan <ayat.online99@hotmail.com>
2022-12-04 13:52:45 +0200

    need to handle memo hook

--- src/pages/view/price-bar.jsx
+++ src/pages/view/price-bar.jsx
@@ -1,9 +1,9 @@
 // import { CartContext } from "../../components/providers/cart-provider";
 import React, { useContext } from 'react';
 
-import './price-bar.css'
+import './price-bar.css';
 
-const Price = (props) => { 
+const Price = (props) => {
 
   // const cartContext = useContext(CartContext);
   const handelIcrement = () => {
@@ -20,14 +20,14 @@ const Price = (props) => {
 
 
   return (
-    <div class='ctrl'>
-  <button class='ctrl__button ctrl__button--decrement'  onClick={handelIcrement}>+</button>
-  <div class='ctrl__counter'>
-    <input class='ctrl__counter-input ctrl__counter-num' type="number" max={500} value={props.cartQuantity} disabled/>
-    {/* <div class='ctrl__counter-num'>0</div> */}
-  </div>
-  <button class='ctrl__button ctrl__button--increment' onClick={handelDecrement}>-</button>
-</div>
-   );
+    <div className='ctrl'>
+      <button className='ctrl__button ctrl__button--decrement' onClick={handelIcrement}>+</button>
+      <div className='ctrl__counter'>
+        <input className='ctrl__counter-input ctrl__counter-num' type="number" max={500} value={props.cartQuantity} disabled />
+        {/* <div className='ctrl__counter-num'>0</div> */}
+      </div>
+      <button className='ctrl__button ctrl__button--increment' onClick={handelDecrement}>-</button>
+    </div>
+  );
 };
 export default Price;
