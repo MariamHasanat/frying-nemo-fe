31e34a84b8e54782ca909f20d9110e17741882d5
abdullah <181004@ppu.edu.ps>
2022-12-04 11:30:11 +0200

    solve warning

--- src/services/users.js
+++ src/services/users.js
@@ -5,8 +5,7 @@
  * @param {string} password
  */
 const loginUser = (email, password) => {
-  return fetch('https://6385ec80beaa6458266d44f1.mockapi.io/nemo/users').
-  then(async response =>{
+  return fetch('https://6385ec80beaa6458266d44f1.mockapi.io/nemo/users').then(async response =>{
     const users = await response.json();
     const user = users.find(user => user.email === email && user.password === password);
     return user || null;
