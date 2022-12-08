2ab0fc458470666ea1101b825777e594483becf1
qjabari <qjabari@sadasol.com>
2022-12-08 12:21:01 +0200

    Cleanup

--- src/pages/add/add.page.jsx
+++ src/pages/add/add.page.jsx
@@ -3,10 +3,8 @@ import './add.css';
 import Form from '../../components/add/form/form.component';
 
 const AddPage = (props) => {
-
   return (
     <div className="add-page">
-      {/* <span className="clock">&#128337;{time.toLocaleTimeString()}</span> */}
       <h1>Add Menu Item</h1>
       <Form onNavigate={props.onNavigate} />
     </div>
