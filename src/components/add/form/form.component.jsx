e01be4092a50b51dade03c16a223a4251519ebbe
raghad__ns <raghadnadersalem20022017@gmail.com>
2022-12-30 23:09:41 +0200

    useAddItem hook

--- src/components/add/form/form.component.jsx
+++ src/components/add/form/form.component.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Input from '../../common/input/input.component';
 import MultivalueInput from '../../common/multivalue-input/multivalue-input/multivalue-input.component';
 import Select from '../../common/select/select.component';
@@ -8,75 +6,22 @@ import './form.css';
 import CATEGORIES from '../../../data/constants';
 import { useContext } from 'react';
 import { UserContext } from '../../../components/providers/user-provider.component';
-import {createItem} from '../../../services/view/fetch-items.service';
+import { useAddItem } from '../../../hooks/add-item.hook';
 
-const Form = (props) => {
-  const [name, setName] = useState('');
-  const [ingredients, setIngredients] = useState([]);
-  const navigate = useNavigate();
+const Form = () => {
+  
   const userContext = useContext(UserContext);
-  /*
-   * calls JSDoc
-   * @param {React.ChangeEvent<HTMLInputElement>} e   //Event object
-   */
-  const handle = async e => {
-    e.preventDefault();
-    console.log('Form Submitted');
-    const price = e.target.price.value;
-    const description = e.target.description.value;
-    const category = e.target.category.value;
-    const image = e.target.image.value;
-
-    const menueItem = {
-      id: Date.now(),
-      name: name,
-      price: price.toString(),
-      description: description,
-      category: category,
-      ingredients: ingredients,
-      image: image
-    };
-    // const itemsJson = localStorage.getItem('menuItems') || '[]';
-    // const items = JSON.parse(itemsJson);
-    // items.push(menueItem);
-    // localStorage.setItem('menuItems', JSON.stringify(items));
-    // navigate('/view');   // to change url without link , without needing to render it as a link component
-
-    const response = await createItem (menueItem) ;
-    if (response) {
-      alert ("item added successfully") ;
-      navigate('/view') ;
-    }
-    else {
-      alert ('something went wrong , can\'t add the item');
-    }
-  };
-  /**
-   * 
-   * @param {React.ChangeEvent<HTMLInputElement>} e 
-   */
-  const nameChange = e => {
-    let val = e.target.value;
-    if (/find/ig.test(val)) {
-      alert('find not allowed to be included in the input !');
-      val = val.replace(/find/ig, 'fry');
-    }
-    if (val.length > 20) {
-
-      alert('character limit exceeded');
-      val = val.substring(0, 20);
-    }
-    setName(val);
-  };
+  const add = useAddItem() ;
+  
   // const CATEGORIES = ["Salads", "Main Dishes", "Drinks", "Sweets"];
   return (
-    <form className='addForm' onSubmit={handle} action='/view'>
+    <form className='addForm' onSubmit={add.submit} action='/view'>
       <Input
         className='input-group'
         label="Name"
-        value={name}
+        value={add.name.value}
         required
-        onChange={nameChange}
+        onChange={add.name.onChange}
       />
       <Textarea
         name='description'
@@ -101,8 +46,8 @@ const Form = (props) => {
       </Select>
       <MultivalueInput
         label='Ingredients' name='ingredients'
-        onChange={item => setIngredients(item)}
-        value={ingredients}
+        onChange={item => add.ingredients.onChange(item)}
+        value={add.ingredients.value}
       />
       <div className='addFormButtons'>
         <button
---
+++ src/hooks/add-item.hook.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createItem } from "../services/view/fetch-items.service";
+
+const useAddItem = () => {
+  const [name, setName] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const navigate = useNavigate();
+
+  /*
+   * calls JSDoc
+   * @param {React.ChangeEvent<HTMLInputElement>} e   //Event object
+   */
+  const handle = async e => {
+    e.preventDefault();
+    console.log('Form Submitted');
+    const price = e.target.price.value;
+    const description = e.target.description.value;
+    const category = e.target.category.value;
+    const image = e.target.image.value;
+
+    const menueItem = {
+      id: Date.now(),
+      name: name,
+      price: price.toString(),
+      description: description,
+      category: category,
+      ingredients: ingredients,
+      image: image
+    };
+    // const itemsJson = localStorage.getItem('menuItems') || '[]';
+    // const items = JSON.parse(itemsJson);
+    // items.push(menueItem);
+    // localStorage.setItem('menuItems', JSON.stringify(items));
+    // navigate('/view');   // to change url without link , without needing to render it as a link component
+
+    const response = await createItem(menueItem);
+    if (response) {
+      alert("item added successfully");
+      navigate('/view');
+    }
+    else {
+      alert('something went wrong , can\'t add the item');
+    }
+  };
+
+  /**
+   * 
+   * @param {React.ChangeEvent<HTMLInputElement>} e 
+   */
+  const nameChange = e => {
+    let val = e.target.value;
+    if (/find/ig.test(val)) {
+      alert('find not allowed to be included in the input !');
+      val = val.replace(/find/ig, 'fry');
+    }
+    if (val.length > 20) {
+
+      alert('character limit exceeded');
+      val = val.substring(0, 20);
+    }
+    setName(val);
+  };
+
+  return {
+    name: {
+      value: name,
+      onChange: nameChange
+    },
+    ingredients: {
+      value: ingredients,
+      onChange: setIngredients
+    },
+    submit: handle
+  };
+};
+export { useAddItem };
