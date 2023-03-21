61c71cd8d417d6cb1aaa00c32ee9640c2f50cf0e
Manolya Tamimi <tamimimanolya@gmail.com>
2023-03-21 13:39:52 +0200

    changing link to backend

--- src/services/fetchItem.js
+++ src/services/fetchItem.js
@@ -1,12 +1,10 @@
-const menuURL = 'https://6385ec80beaa6458266d44f1.mockapi.io/nemo/menu/';
+const menuURL = `${process.env.REACT_APP_SERVER_URL}/menu/`;
 
 /**
  * Fake Fetching of single item
  * @param {number} id 
  */
 const getItem = (myId) => {
-    // const items = JSON.parse(localStorage.getItem('menu'));
-    // return items.filter(item => item.id.toString() === myId)[0] || null;
     const url = menuURL + myId;
     return fetch(url)
         .then(res => res.json())
