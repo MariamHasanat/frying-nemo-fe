7380c0a973ddcf3587ca0c393a49e65465e6a635
ruba <ruba@gmail.com>
2023-03-21 13:43:53 +0200

    add searchTerm

--- src/hooks/menu/get-items.hook.js
+++ src/hooks/menu/get-items.hook.js
@@ -1,18 +1,19 @@
 //fetch items from server
 import { useEffect, useState } from "react";
 import { fetchItems } from "../../services/item";
-
+import useParams from "../params.hook";
 
 const useGetItem = () =>{
   const [state, setState] = useState({menuItems: [], loading: true});
+  const {myParams} = useParams();
 
   const getMenuItems = async () => {
     setState({...state,loading: true});
-    const items = await fetchItems();
+    const items = await fetchItems(myParams.searchTermsFromURL)
     setState({loading: false, menuItems: items});
 };
 
-useEffect(()=> {getMenuItems(); }, []);
+useEffect(()=> {getMenuItems(); }, [myParams]);
   return {
     ...state
   }
