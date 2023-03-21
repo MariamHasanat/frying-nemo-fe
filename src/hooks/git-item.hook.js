9512d51d31d0672622b8afb496b1b0e7db605c7f
sajedaswad <you@example.com>
2023-03-21 12:53:24 +0200

    delete filter and git hook that get frpm be

---
+++ src/hooks/git-item.hook.js
@@ -0,0 +1,22 @@
+/*
+* This hooks is used to fetch items form the server (all items or filtered), 
+* and to store the state of the menu items
+*/
+
+
+import {useState , useEffect } from 'react'
+import { fetchItemsApi } from '../components/add/form/data/items';
+ const useGetitems =()=>{
+    const [state ,setState ]= useState({menuItems:[],loading:true});
+    const getMenuItems = async () => {
+        setState ({...state,loading:true});
+     
+        const items = await fetchItemsApi();
+        setState ({menuItems:items,loading:false});
+      };
+
+      useEffect (()=>{getMenuItems()},[]);
+      
+      return {...state};
+ }
+ export default useGetitems;
--- src/pages/add/view/view.jsx
+++ src/pages/add/view/view.jsx
@@ -1,42 +1,25 @@
 import { useEffect, useState } from 'react';
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
-import Input from '../../../components/common/input/input';
 import Item from '../../../components/item/items/item';
 import Spinner from '../../../components/spinner/spinner';
 import './view.css';
-import Select from '../../../components/common/select/select';
-import FilterBar from '../../../components/filter-bar/filter-bar';
+// import FilterBar from '../../../components/filter-bar/filter-bar';
 import { useContext } from 'react';
 import { UserContext } from '../../../components/provider/provider';
 import { getQuantity } from '../../../util/util';
 import { CartContext } from '../../../components/provider/cartprovider';
-import { fetchItemsApi, getItemsApi } from '../../../components/add/form/data/items';
-import useFilterItems from '../../../hooks/filterItems.hook';
-import useSearchItem from '../../../hooks/searchItem.hook';
+import useGetitems from '../../../hooks/git-item.hook';
 
-/**
- * @type {Array<{
- * name: string;
- * description: string;
- * ingredients: string[];
- * price: number;
- * category: string;
- * image: string;
- * }>}
- */
-const initialItems = [];
 const View = () => {
   const cartContext = useContext(CartContext);
-  const [menuItem, setMenuItem] = useState(initialItems);
-  const [loading, setLoading] = useState(true);
   const userContext = useContext(UserContext);
-  const  [min,setMin] = useState();
-  const  [max,setMax] = useState();
+  // const  [min,setMin] = useState();
+  // const  [max,setMax] = useState();
   const navigate = useNavigate();
   // const setItemSearchInLocalStoreg=(value)=>{
   //   localStorage.setItem("the informathion search",value);
-  //   SetSearch(value);[]F
+  //   SetSearch(value);[]
 
   // }
   useEffect(() => {
@@ -44,40 +27,29 @@ const View = () => {
       navigate('/login');
     }
   }, []);
-  const {myParam,setParam}= useSearchItem();
-
-  const filterItem = useFilterItems(menuItem, {max, min });
-
-
-  const getMenuItems = async () => {
-    setLoading(true);
-    const items = await fetchItemsApi();
-    setMenuItem(items);
-    setLoading(false);
+  const { loading, menuItems } = useGetitems();
+  // const filterItem = useFilterItems(menuItems, {max, min });
 
 
-  };
-  useEffect(() => {
-    getMenuItems();
-  }, []);
+
 
   return (
     <div className='view-page'>
       <h1>View menu item </h1>
-      <FilterBar
+      {/* <FilterBar
        
         setMax={setMax}
         setMin={setMin}
       >
-      </FilterBar >
+      </FilterBar > */}
       {
         loading
           ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Spinner /></div>
           : (
             <div className="items-container">
               {
-                filterItem.length
-                  ? filterItem.map((item, index) =>
+                menuItems.length
+                  ? menuItems.map((item, index) =>
                     <Item
                       data={item}
                       key={item.name + index}
